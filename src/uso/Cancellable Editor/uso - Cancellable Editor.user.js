4d8130d30d8b827fa4bb2426cc853d94a61f678c
Martii <thalamew@hotmail.com>
2009-07-04 22:19:31 -0600

    48200.103180 uso - Cancellable Editor [0.0.3.1](/scripts/diff/48200/103180)
    --- Trying out the [@require updater](/guides/16)α... One confirmed bug already in [r20](http://code.google.com/p/usoupdater/source/detail?r=20)
    --- Trying out the [@require updater](/guides/16)α [r27](http://code.google.com/p/usoupdater/source/detail?r=27)
    --- Trying out the [@require updater](/guides/16)α [r30](http://code.google.com/p/usoupdater/source/detail?r=30)
    
    48200.103180
    
    SHA1:a1961ccce0645980cd34117596d983d3a4d474ee

--- src/uso/Cancellable Editor/uso - Cancellable Editor.user.js
+++ src/uso/Cancellable Editor/uso - Cancellable Editor.user.js
@@ -8,9 +8,10 @@ var fileMETA = <><![CDATA[
 // @copyright     2009+, Marti Martz (http://userscripts.org/users/37004)
 // @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @license       (CC) Attribution Non-Commercial Share Alike; http://creativecommons.org/licenses/by-nc-sa/3.0/
-// @version       0.0.3
+// @version       0.0.3.1
 // @include       http://userscripts.org/topics/*
 // @include       https://userscripts.org/topics/*
+// @require       http://updater.usotools.co.cc/48200.js?e4x=true
 // ==/UserScript==
 ]]></>;
 
@@ -46,7 +47,7 @@ http://userscripts.org/topics/26205
   }
   //  ***************************************************************************
 
-  if (typeof unsafeWindow == "object" && typeof unsafeWindow.Prototype == "object")
+  if (typeof unsafeWindow == "object" && typeof unsafeWindow.jQuery == "function")
     return;
   
   if (document.evaluate(
