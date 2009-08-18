8d6d82930d9229eb23212617f016b4c477b29f03
Martii <thalamew@hotmail.com>
2009-08-18 16:39:56 -0600

    48071.115076 uso - screen.css [0.0.2](/scripts/diff/48071/115076) - Incremented version
    48071.115076
    
    SHA1:69c9c688aca9ae0c4867bfd0f647c66585e73527

--- src/uso/screen.css/uso - screen.css.user.js
+++ src/uso/screen.css/uso - screen.css.user.js
@@ -4,7 +4,7 @@
 // @description   Corrects any discovered issues with screen.css that come into my scope until uso gets around to fixing and sometimes it will do nothing.
 // @copyright     2009+, Marti Martz (http://userscripts.org/users/37004)
 // @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
-// @version       0.0.1
+// @version       0.0.2
 // @include       http://userscripts.org/*
 // @require       http://usocheckup.dune.net/index.php?scriptid=48071
 // ==/UserScript==
