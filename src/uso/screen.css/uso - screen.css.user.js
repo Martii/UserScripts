6b76b3123f699cbfce38c17b71574534062478ce
Martii <thalamew@hotmail.com>
2009-09-28 00:27:22 -0600

    48071.127544 uso - screen.css [0.0.3](/scripts/diff/48071/127544)
    - Using friendly URI for updater
    
    48071.127544
    
    SHA1:56ddafde476f259e12837bd89d1d5b7a8d327e5f

--- src/uso/screen.css/uso - screen.css.user.js
+++ src/uso/screen.css/uso - screen.css.user.js
@@ -4,9 +4,9 @@
 // @description   Corrects any discovered issues with screen.css that come into my scope until uso gets around to fixing and sometimes it will do nothing.
 // @copyright     2009+, Marti Martz (http://userscripts.org/users/37004)
 // @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
-// @version       0.0.2
+// @version       0.0.3
 // @include       http://userscripts.org/*
-// @require       http://usocheckup.dune.net/index.php?scriptid=48071
+// @require       http://usocheckup.dune.net/48071.js
 // ==/UserScript==
 
 (function() {
