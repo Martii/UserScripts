450db2a3245e34f4d6304ac6e34090a72458da78
Martii <thalamew@hotmail.com>
2010-02-07 14:34:32 -0700

    68219.171159 uso - installWith [0.0.7](/scripts/diff/68219/171159)
    - Decreased include scope.
    
    68219.171159
    
    8939910e9a6853c00321ab324c226525c1c70ea8

--- src/uso/installWith/uso - installWith.user.js
+++ src/uso/installWith/uso - installWith.user.js
@@ -7,11 +7,9 @@
 // @copyright     2010+, Marti Martz (http://userscripts.org/users/37004)
 // @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @license       Creative Commons; http://creativecommons.org/licenses/by-nc-nd/3.0/
-// @version       0.0.6
-// @include   http://userscripts.org/scripts/*/*
-// @include   https://userscripts.org/scripts/*/*
-// @include   http://userscripts.org/topics/*
-// @include   https://userscripts.org/topics/*
+// @version       0.0.7
+// @include   http://userscripts.org/scripts/show/*
+// @include   https://userscripts.org/scripts/show/*
 // ==/UserScript==
 
   function getScriptid() {
