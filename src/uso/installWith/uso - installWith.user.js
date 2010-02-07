15a09f19d7caa29a3aee15ecbd64f299a1db6964
Martii <thalamew@hotmail.com>
2010-02-07 03:13:31 -0700

    68219.170962 uso - installWith [0.0.5](/scripts/diff/68219/170962)
    - Disabled script via alert box while fixing backend... major issues.
    
    68219.170962
    
    700bef9d3d9125f711a551ae09aecd2ad5caa3ad

--- src/uso/installWith/uso - installWith.user.js
+++ src/uso/installWith/uso - installWith.user.js
@@ -7,12 +7,15 @@
 // @copyright     2010+, Marti Martz (http://userscripts.org/users/37004)
 // @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @license       Creative Commons; http://creativecommons.org/licenses/by-nc-nd/3.0/
-// @version       0.0.4
+// @version       0.0.5
 // @include   http://userscripts.org/scripts/*/*
 // @include   https://userscripts.org/scripts/*/*
 // @include   http://userscripts.org/topics/*
 // @include   https://userscripts.org/topics/*
 // ==/UserScript==
+
+alert("This script is disabled.  Please check again at a later time for a new update");
+return;
 
   function getScriptid() {
     var scriptid = window.location.pathname.match(/\/scripts\/.+\/(\d+)/i);
