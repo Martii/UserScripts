6156ed2a6d0df61d66368a27135bcd313aa3af04
Martii <thalamew@hotmail.com>
2010-02-11 16:27:28 -0700

    68219.173024 uso - installWith [0.0.19](/scripts/diff/68219/173024)
    - Changing rex to ignore null @ matches
    
    68219.173024
    
    476d92ca6afa054cede2bd34058542e70c95ae14

--- src/uso/installWith/uso - installWith.user.js
+++ src/uso/installWith/uso - installWith.user.js
@@ -7,7 +7,7 @@
 // @copyright     2010+, Marti Martz (http://userscripts.org/users/37004)
 // @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @license       Creative Commons; http://creativecommons.org/licenses/by-nc-nd/3.0/
-// @version       0.0.18
+// @version       0.0.19
 // @include http://userscripts.org/scripts/show/*
 // @include https://userscripts.org/scripts/show/*
 // @require http://usocheckup.dune.net/68219.js?method=install&open=window&maxage=14&custom=yes&topicid=45479&id=usoCheckup
@@ -73,7 +73,7 @@
               var line, name, prefix, header, key, value;
                 var lines = metadataBlock.split(/\n/).filter(/\/\/ @/);
                 for each (line in lines) {
-                  [, name, value] = line.match(/\/\/ @(\S+)\s*(.*)/);
+                  [, name, value] = line.match(/\/\/ @(\S*)\s*(.*)/);
                   switch (name) {
                     case "licence":
                       name = "license";
