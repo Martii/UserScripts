b37ba3f5635c0977ecdaa38bb69ad47ae7e56bd1
Martii <thalamew@hotmail.com>
2010-02-18 05:46:33 -0700

    69307.175677 uso - Count Issues [0.0.0](/scripts/diff/69307/175677)
    - Initial metadata block commit
    
    Building this script in response to [this topic](/topics/47687) for a valid implementation.
    
    69307.175677
    
    fa94425556e1b77dc78cb1c36e9c580737d0213a

---
+++ src/uso/Count Issues/uso - Count Issues.user.js
@@ -0,0 +1,18 @@
+(function() {
+
+// ==UserScript==
+// @name          uso - countIssues
+// @namespace     http://userscripts.org/users/37004
+// @description   Counts the issues and places the appropriate count on the Issues tab
+// @copyright     2010+, Marti Martz (http://userscripts.org/users/37004)
+// @contributor   sizzlemctwizzle (http://userscripts.org/users/27715)
+// @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
+// @license       Creative Commons; http://creativecommons.org/licenses/by-nc-nd/3.0/
+// @version       0.0.0
+// @include   http://userscripts.org/scripts/*/*
+// @include   https://userscripts.org/scripts/*/*
+// @include   http://userscripts.org/topics/*
+// @include   https://userscripts.org/topics/*
+// ==/UserScript==
+
+})();
