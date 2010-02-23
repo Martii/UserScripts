7eeda2b793b7988153e87cbd615f2ee078cd4455
Martii <thalamew@hotmail.com>
2010-02-23 00:15:20 -0700

    69725.177757 uso - Anchor Bookmarks [0.0.1](/scripts/diff/69725/177757)
    - Initial release
    
    69725.177757
    
    SHA1:c5e1954566b36c89e34aefd4ec3d0de7c42f9176

---
+++ src/uso/Anchor Bookmarks/uso - Anchor Bookmarks.user.js
@@ -0,0 +1,44 @@
+(function() {
+
+// ==UserScript==
+// @name          uso - Anchor Bookmarks
+// @namespace     http://userscripts.org/users/37004
+// @description   With null anchor tags, finds next available text node line and converts it into a bookmark
+// @copyright     2010+, Marti Martz (http://userscripts.org/users/37004)
+// @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
+// @license       Creative Commons; http://creativecommons.org/licenses/by-nc-nd/3.0/
+// @version       0.0.1
+// @include   http://userscripts.org/*
+// @include   https://userscripts.org/*
+// ==/UserScript==
+
+  var xpr = document.evaluate(
+    "//a[not(@href)][not(@name)][not(@id)]",
+    document,
+    null,
+    XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
+    null
+  );
+
+  var thisNode, thatNode;
+  if (xpr)
+    for (var i = xpr.snapshotLength - 1; thisNode = xpr.snapshotItem(i); --i) {
+      var thatNode = thisNode;
+      while (thatNode) {
+        if (thatNode.textContent != "") {
+          var bookmark;
+          bookmark = encodeURIComponent(thatNode.textContent.toLowerCase());
+          bookmark = bookmark.replace("%20", "-", "g");
+          bookmark = bookmark.replace("%0A", "", "g");
+          bookmark = bookmark.replace("%", ".", "g");
+          bookmark = "bookmark-" + bookmark;
+
+          thisNode.setAttribute("id", bookmark);
+          thisNode.setAttribute("name", bookmark);
+            break;
+        }
+        thatNode = thatNode.nextSibling;
+      }
+    }
+
+})();
