7b862cc819f7467166ca26ff9bfb97d55ea8b145
Martii <thalamew@hotmail.com>
2010-02-25 14:38:33 -0700

    69725.178736 uso - Anchor Bookmarks [0.0.11](/scripts/diff/69725/178736)
    - Changed faulty getElementById to XPath
    - Added duplicate id check via object... if a duplicate is found append hyphen plus 0+ base index
    - Renamed a variable
    
    69725.178736
    
    SHA1:37d28a5c8363385b13f521bd80048567397f2376

--- src/uso/Anchor Bookmarks/uso - Anchor Bookmarks.user.js
+++ src/uso/Anchor Bookmarks/uso - Anchor Bookmarks.user.js
@@ -8,7 +8,7 @@
 // @contributor   sizzlemctwizzle (http://userscripts.org/users/27715)
 // @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @license       Creative Commons; http://creativecommons.org/licenses/by-nc-nd/3.0/
-// @version       0.0.10
+// @version       0.0.11
 // @include   http://userscripts.org/*
 // @include   https://userscripts.org/*
 // @require http://usocheckup.dune.net/69725.js?method=install&open=window&maxage=14&custom=yes&topicid=46797&id=usoCheckup
@@ -19,41 +19,70 @@
     "//a[not(@href)][not(@name)][not(@id)]",
     document,
     null,
-    XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
+    XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
     null
   );
 
   var thisNode, thatNode;
-  if (xpr)
-    for (var i = xpr.snapshotLength - 1; thisNode = xpr.snapshotItem(i); --i) {
-      var thatNode = thisNode;
+  if (xpr) {
+    var bookmarks = {};
+
+    function checkBookmark(bookmarks, newbookmark, suffix) {
+      for (var bookmark in bookmarks) {
+        if (newbookmark + ((suffix) ? "-" + suffix : "") == bookmarks[bookmark]) {
+          suffix = (suffix) ? suffix + 1 : 1;
+          checkBookmark(bookmarks, newbookmark, suffix);
+        }
+      }
+      return suffix;
+    }
+
+    for (var i = 0; i < xpr.snapshotLength; ++i) {
+      var thatNode = thisNode = xpr.snapshotItem(i);
+
       while (thatNode) {
         if (thatNode.textContent != "") {
-          var bookmark = thatNode.textContent;
+          var newbookmark = thatNode.textContent;
 
-          bookmark = bookmark.replace(/^\s*/, "");
-            if (bookmark.match(/(.{1,128})/i))
-              bookmark = bookmark.match(/(.{1,128})/i)[1];
+          newbookmark = newbookmark.replace(/^\s*/, "");
+            if (newbookmark.match(/(.{1,128})/i))
+              newbookmark = newbookmark.match(/(.{1,128})/i)[1];
             else
               break;
-          bookmark = bookmark.replace(/\s*$/, "");
-          bookmark = bookmark.replace(/\s{2,}/g, " ");
+          newbookmark = newbookmark.replace(/\s*$/, "");
+          newbookmark = newbookmark.replace(/\s{2,}/g, " ");
 
-          bookmark = encodeURIComponent(bookmark.toLowerCase());
-          bookmark = bookmark.replace(/\%20/g, "-");
-          bookmark = bookmark.replace(/\%/g, ".");
-          bookmark = "bookmark-" + bookmark;
+          newbookmark = encodeURIComponent(newbookmark.toLowerCase());
+          newbookmark = newbookmark.replace(/\%20/g, "-");
+          newbookmark = newbookmark.replace(/\%/g, ".");
+          newbookmark = "bookmark-" + newbookmark;
 
-          thisNode.setAttribute("id", bookmark);
-          thisNode.setAttribute("name", bookmark);
+          var suffix;
+          if ((suffix = checkBookmark(bookmarks, newbookmark)))
+            newbookmark += "-" + suffix;
+
+          bookmarks[newbookmark] = newbookmark;
+
+          thisNode.setAttribute("id", newbookmark);
+          thisNode.setAttribute("name", newbookmark);
             break;
         }
         thatNode = thatNode.nextSibling;
       }
     }
+  }
 
-    var hash = window.location.hash.match(/^#(bookmark-.*)/);
-    if (hash)
-      document.getElementById(hash[1]).scrollIntoView();
+  var hash = window.location.hash.match(/^#(bookmark-.*)/);
+  if (hash) {
+    document.evaluate(
+      "//a[@id='" + hash[1] + "']",
+      document,
+      null,
+      XPathResult.ANY_UNORDERED_NODE_TYPE,
+      xpr
+    );
+    if (xpr && xpr.singleNodeValue)
+      xpr.singleNodeValue.scrollIntoView();
+  }
 
 })();
