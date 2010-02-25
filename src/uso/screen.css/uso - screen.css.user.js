a26ecfabfecbc59bcd98df82dc4e3a1e42f3849c
Martii <thalamew@hotmail.com>
2010-02-25 12:01:46 -0700

    48071.178684 uso - screen.css [0.0.4](/scripts/diff/48071/178684)
    - Temporary fix for script icons using large instead of thumb and removed width attribute to prevent scaling from [this topic](/topics/27335?page=2#posts-224051). Note any custom script icon that is below the 120px x 90px will be scaled up as that is the nature of s3.  [Example script](/scripts/show/68853)
    
    48071.178684
    
    SHA1:ef0e4a77108aac421538d553c8a2ce28c3368034

--- src/uso/screen.css/uso - screen.css.user.js
+++ src/uso/screen.css/uso - screen.css.user.js
@@ -4,13 +4,14 @@
 // @description   Corrects any discovered issues with screen.css that come into my scope until uso gets around to fixing and sometimes it will do nothing.
 // @copyright     2009+, Marti Martz (http://userscripts.org/users/37004)
 // @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
-// @version       0.0.3
+// @version       0.0.4
 // @include       http://userscripts.org/*
 // @require       http://usocheckup.dune.net/48071.js
 // ==/UserScript==
 
 (function() {
 
+  // Right to left fix
   var CSS = <><![CDATA[
 
     td { text-align: inherit; }
@@ -18,5 +19,28 @@
   ]]></> + "";
 
   GM_addStyle(CSS);
+
+  // Script icons if custom... change back to thumb
+  if (window.location.pathname.match(/(.+)scripts\/show\/\d+/)) {
+    var iconNode = document.evaluate(
+      "//a[@id='icon']/img",
+      document,
+      null,
+      XPathResult.ANY_UNORDERED_NODE_TYPE,
+      null
+    );
+
+    if (iconNode && iconNode.singleNodeValue) {
+      var thisNode = iconNode.singleNodeValue;
+
+      var url = thisNode.getAttribute("src");
+      if (url.match(/http:\/\/s3\.amazonaws\.com\/uso_ss\/icon\/\d+\/(large)\.png\?\d+/)) {
+        url = url.replace("large", "thumb");
+
+        thisNode.removeAttribute("width");
+        thisNode.setAttribute("src", url);
+      }
+    }
+  }
 
 })();
