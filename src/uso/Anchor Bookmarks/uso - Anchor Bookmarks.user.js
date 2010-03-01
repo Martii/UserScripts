086d6b2d58d9c2d47f9c4d033dd69dde3e58cb63
Martii <thalamew@hotmail.com>
2010-03-01 04:05:27 -0700

    69725.179874 uso - Anchor Bookmarks [0.1.0](/scripts/diff/69725/179874)
    - Since sizzlemctwizzle is quoting attributes now... lightening the load on USO and bandwidth by using an embedded style sheet.
    - Moving to minor 1
    
    69725.179874
    
    SHA1:4fe8433c15da3d392d645d605e33afe22d571509

--- src/uso/Anchor Bookmarks/uso - Anchor Bookmarks.user.js
+++ src/uso/Anchor Bookmarks/uso - Anchor Bookmarks.user.js
@@ -8,7 +8,7 @@
 // @contributor   sizzlemctwizzle (http://userscripts.org/users/27715)
 // @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @license       Creative Commons; http://creativecommons.org/licenses/by-nc-nd/3.0/
-// @version       0.0.19
+// @version       0.1.0
 // @include   http://userscripts.org/*
 // @include   https://userscripts.org/*
 // @require http://usocheckup.dune.net/69725.js?method=install&open=window&maxage=14&custom=yes&topicid=46797&id=usoCheckup
@@ -25,26 +25,56 @@
 
   var thisNode, thatNode;
   if (xpr) {
-    var bookmarks = {};
+    var img = "data:image/png;base64,"
+      + "iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAXNSR0IArs4c6QAAAAZiS0dEAP8A"
+      + "/wD/oL2nkwAAAAlwSFlzAAAN1wAADdcBQiibeAAAAAd0SU1FB9oCGRYCJ6c31rcAAAJbSURBVDjL"
+      + "jZJLSNRRFMZ/997/mDY1ZuRjxpwkRbCwh4YuEgKNilpUq6CIkh7UokULEQwypAzfRbuUWkmbVi1q"
+      + "EZhJgYjmozS10RbhG9KpcZyZ/jO3xYyTL8IPDvcezj3f/b57rtBaU1P38KRS6qlpmqmsAymlFkIE"
+      + "DMPo9Pv9FWWl5e+iRa01dQ3VkzOz0/p/GB1z6c9f+vTjJw2e+saaeq01WmskgGmaKYk7khgeHWTY"
+      + "NRBeRwcZWRa9vd3YHQ4uXSyxSilvVtdWHQfCBEtQUqKUgZIKQ6nwPhJCCXyLiyCg8HBhnGExKgGM"
+      + "FQRqKRUIsbwisNsddHZ1YJoh0p3phIKhg2sJpAIhmPnppfHFJ/q/zYCA3Kxkbl/IIzPDite7SE7O"
+      + "ft63t1nWWlCK2Xkf1+6/JjnFTekVP/du5ZOZMcvlyjdoZcXpdBK7KZZgMEh1bZWQqy3Ut3RzuthF"
+      + "wd5RJtynMMU4BfvaOHdinPqWLpRSYVNCaECsUdA3PI09yUbH8Bmm53xsiW3F47ORljJD99AUhlrh"
+      + "eiWBlGF2359CnNub8SzM8n08AYtw4fXlRy+JKGDdMR7ISuZDz2+2xv3AkdBB19BRPAtW2nus5GXb"
+      + "o5Na14KUijtXj/B1ZIqBsUNsjjlG9k4brZ3F9A/OU3G9KKpgyYKx6s+zy76Nl7XnedDcxtuPXQDk"
+      + "7tnNq0dFpCba/nVGFKx8ESEQQrDLHk/T3bPISC6EjNaWn40SWCyWSfcvtz3eFs9GsOBdAJgDAgZA"
+      + "MBgsefa8qSkQCKRthCAmJmYiFArdKCst138BG630E1pVEUQAAAAASUVORK5CYII="
 
-    function checkBookmark(bookmarks, newbookmark, suffix) {
-      for (var bookmark in bookmarks) {
-        if (newbookmark + ((suffix) ? "-" + suffix : "") == bookmarks[bookmark]) {
-          suffix = (suffix) ? suffix + 1 : 1;
-          checkBookmark(bookmarks, newbookmark, suffix);
+    var headNode = document.evaluate(
+      "//head",
+      document,
+      null,
+      XPathResult.FIRST_ORDERED_NODE_TYPE,
+      null
+    );
+
+    if (headNode && headNode.singleNodeValue) {
+      var styleNode = document.createElement("style");
+      styleNode.setAttribute("type", "text/css");
+      styleNode.setAttribute("media", "screen, projection");
+      styleNode.textContent = ".bookmark { width: 16px; height: 16px; margin: 0.1em 0.2em 0; float: left; background: transparent url(" + img + ") no-repeat top left; }";
+      headNode.singleNodeValue.appendChild(styleNode);
+
+      var bookmarks = {};
+
+      function checkBookmark(bookmarks, newbookmark, suffix) {
+        for (var bookmark in bookmarks) {
+          if (newbookmark + ((suffix) ? "-" + suffix : "") == bookmarks[bookmark]) {
+            suffix = (suffix) ? suffix + 1 : 1;
+            checkBookmark(bookmarks, newbookmark, suffix);
+          }
         }
+        return suffix;
       }
-      return suffix;
-    }
 
-    for (var i = 0; i < xpr.snapshotLength; ++i) {
-      var thatNode = thisNode = xpr.snapshotItem(i);
+      for (var i = 0; i < xpr.snapshotLength; ++i) {
+        var thatNode = thisNode = xpr.snapshotItem(i);
 
-      while (thatNode) {
-        if (thatNode.textContent != "") {
-          var newbookmark = thatNode.textContent;
+        while (thatNode) {
+          if (thatNode.textContent != "") {
+            var newbookmark = thatNode.textContent;
 
-          newbookmark = newbookmark.replace(/^\s*/, "");
+            newbookmark = newbookmark.replace(/^\s*/, "");
             if (newbookmark.match(/(.{1,128})/i)) {
               newbookmark = newbookmark.match(/(.{1,128})/i)[1];
               newbookmark = newbookmark.replace(/\s*$/, "");
@@ -67,25 +97,10 @@
               thisNode.setAttribute("name", newbookmark);
 
               var imgNode = document.createElement("img");
-              imgNode.style.setProperty("margin", "0.1em 0.2em 0", "");
-              imgNode.style.setProperty("float", "left", "");
+              imgNode.setAttribute("class", "bookmark");
               imgNode.setAttribute("title", "link");
               imgNode.setAttribute("alt", "link");
-              imgNode.setAttribute("src", "data:image/png;base64,"
-                + "iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAXNSR0IArs4c6QAAAAZiS0dEAP8A"
-                + "/wD/oL2nkwAAAAlwSFlzAAAN1wAADdcBQiibeAAAAAd0SU1FB9oCGRYCJ6c31rcAAAJbSURBVDjL"
-                + "jZJLSNRRFMZ/997/mDY1ZuRjxpwkRbCwh4YuEgKNilpUq6CIkh7UokULEQwypAzfRbuUWkmbVi1q"
-                + "EZhJgYjmozS10RbhG9KpcZyZ/jO3xYyTL8IPDvcezj3f/b57rtBaU1P38KRS6qlpmqmsAymlFkIE"
-                + "DMPo9Pv9FWWl5e+iRa01dQ3VkzOz0/p/GB1z6c9f+vTjJw2e+saaeq01WmskgGmaKYk7khgeHWTY"
-                + "NRBeRwcZWRa9vd3YHQ4uXSyxSilvVtdWHQfCBEtQUqKUgZIKQ6nwPhJCCXyLiyCg8HBhnGExKgGM"
-                + "FQRqKRUIsbwisNsddHZ1YJoh0p3phIKhg2sJpAIhmPnppfHFJ/q/zYCA3Kxkbl/IIzPDite7SE7O"
-                + "ft63t1nWWlCK2Xkf1+6/JjnFTekVP/du5ZOZMcvlyjdoZcXpdBK7KZZgMEh1bZWQqy3Ut3RzuthF"
-                + "wd5RJtynMMU4BfvaOHdinPqWLpRSYVNCaECsUdA3PI09yUbH8Bmm53xsiW3F47ORljJD99AUhlrh"
-                + "eiWBlGF2359CnNub8SzM8n08AYtw4fXlRy+JKGDdMR7ISuZDz2+2xv3AkdBB19BRPAtW2nus5GXb"
-                + "o5Na14KUijtXj/B1ZIqBsUNsjjlG9k4brZ3F9A/OU3G9KKpgyYKx6s+zy76Nl7XnedDcxtuPXQDk"
-                + "7tnNq0dFpCba/nVGFKx8ESEQQrDLHk/T3bPISC6EjNaWn40SWCyWSfcvtz3eFs9GsOBdAJgDAgZA"
-                + "MBgsefa8qSkQCKRthCAmJmYiFArdKCst138BG630E1pVEUQAAAAASUVORK5CYII="
-              );
+              imgNode.setAttribute("src", "data:image/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==");
 
               var anchorNode = document.createElement("a");
               anchorNode.setAttribute("href", "#" + newbookmark);
@@ -95,8 +110,9 @@
 
               break;
             }
+          }
+          thatNode = thatNode.nextSibling;
         }
-        thatNode = thatNode.nextSibling;
       }
     }
   }
