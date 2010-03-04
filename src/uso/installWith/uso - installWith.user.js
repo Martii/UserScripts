58a7971f3a0e7444356025060014cb87cd2bca83
Martii <thalamew@hotmail.com>
2010-03-04 12:03:15 -0700

    68219.181023 uso - installWith [0.1.4](/scripts/diff/68219/181023)
    - Added /reviews/* to [@include](http://sf.net/apps/mediawiki/greasemonkey/index.php?title=Metadata_Block#.40include)
    
    68219.181023
    
    a8773ca25b9559bd264ac5eb0667ced7a48349bf

--- src/uso/installWith/uso - installWith.user.js
+++ src/uso/installWith/uso - installWith.user.js
@@ -7,11 +7,13 @@
 // @copyright     2010+, Marti Martz (http://userscripts.org/users/37004)
 // @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @license       Creative Commons; http://creativecommons.org/licenses/by-nc-nd/3.0/
-// @version       0.1.3
+// @version       0.1.4
 // @include   http://userscripts.org/scripts/*/*
 // @include   https://userscripts.org/scripts/*/*
 // @include   http://userscripts.org/topics/*
 // @include   https://userscripts.org/topics/*
+// @include   http://userscripts.org/reviews/*
+// @include   https://userscripts.org/reviews/*
 // @require http://usocheckup.dune.net/68219.js?method=install&open=window&maxage=14&custom=yes&topicid=45479&id=usoCheckup
 // @require http://userscripts.org/scripts/source/61794.user.js
 // ==/UserScript==
