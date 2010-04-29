1dc8be7fde14c78eadc08e1fbe023ec2330764c7
Martii <thalamew@hotmail.com>
2010-04-29 17:20:05 -0600

    68219.203971 uso - installWith [0.3.1](/scripts/diff/68219/203971)
    - 2nd HEAD call to script source causes a 503 error to be returned... Trying cache control...
    
    68219.203971
    
    7145aab0ef2f6a924e31482352c9f7dfb7c261ae

--- src/uso/installWith/uso - installWith.user.js
+++ src/uso/installWith/uso - installWith.user.js
@@ -7,7 +7,7 @@
 // @copyright     2010+, Marti Martz (http://userscripts.org/users/37004)
 // @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @license       Creative Commons; http://creativecommons.org/licenses/by-nc-nd/3.0/
-// @version       0.3.0
+// @version       0.3.1
 // @include http://userscripts.org/scripts/*/*
 // @include https://userscripts.org/scripts/*/*
 // @include http://userscripts.org/topics/*
@@ -117,6 +117,10 @@
   if (scriptid) {
     GM_xmlhttpRequest({
       url: "http://userscripts.org/scripts/source/" + scriptid + ".user.js?",
+      headers: {
+        "Cache-Control": "no-cache"
+        "Pragma": "no-cache"
+      },
       method: "HEAD",
       onload: function(xhr) {
         if (xhr.status != 200) {
