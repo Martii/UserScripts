e7f7c4f6d5e11b8848579cfe17d6830860a4d176
Martii <thalamew@hotmail.com>
2010-08-02 23:24:10 -0600

    69307.236683 uso - Count Issues [0.4.3](/scripts/diff/69307/236683)
    - Typo and reverting blank namespace
    
    69307.236683
    
    cfbc57d0b407aa7262184038db1dddaab384ec8c

--- src/uso/Count Issues/uso - Count Issues.user.js
+++ src/uso/Count Issues/uso - Count Issues.user.js
@@ -8,7 +8,7 @@
 // @contributor   sizzlemctwizzle (http://userscripts.org/users/27715)
 // @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @license       Creative Commons; http://creativecommons.org/licenses/by-nc-nd/3.0/
-// @version       0.4.2
+// @version       0.4.3
 //
 // @include   http://userscripts.org/scripts/*/*
 // @include   https://userscripts.org/scripts/*/*
@@ -507,7 +507,7 @@
                       if (headers[key])
                         display(mbx, headers[key], key, "Namespaces");
                       else
-                        display(mbx, "", key, "Namespace", true);
+                        display(mbx, "userscripts.org", key, "Namespaces", true);
                       break;
                     case "description":
                       if (headers[key] && summaryNode
