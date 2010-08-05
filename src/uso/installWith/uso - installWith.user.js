15bd4430fc0ab248bddbbb8c4f9c43240a8fb4a9
Martii <thalamew@hotmail.com>
2010-08-05 01:46:46 -0600

    68219.237429 uso - installWith [0.5.6](/scripts/diff/68219/237429)
    - Screwed this one up
    
    68219.237429
    
    6ba10f160996bbd11c2e754ff48f11ea71259a91

--- src/uso/installWith/uso - installWith.user.js
+++ src/uso/installWith/uso - installWith.user.js
@@ -7,7 +7,7 @@
 // @copyright     2010+, Marti Martz (http://userscripts.org/users/37004)
 // @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @license       Creative Commons; http://creativecommons.org/licenses/by-nc-nd/3.0/
-// @version       0.5.5
+// @version       0.5.6
 // @include http://userscripts.org/scripts/*/*
 // @include https://userscripts.org/scripts/*/*
 // @include http://userscripts.org/topics/*
@@ -496,13 +496,30 @@
                       "updater": "33024",
                       "rex": [
                         "^https?:\\/\\/userscripts\\.org\\/scripts\\/source\\/33024\\.user\\.js",
-                        "^https?:\\/\\/userscripts\\.org\\/scripts\\/version\\/\d+\\/33024\\.user\\.js"
+                        "^https?:\\/\\/userscripts\\.org\\/scripts\\/version\\/\\d+\\/33024\\.user\\.js"
                       ],
                       "url": "",
                       "qs": "",
                       "securityAdvisory": {
                         "advisory": "guarded",
                         "title": ", Top-level script may have update check init for this lib"
+                      }
+                    },
+                    "88773": {
+                      "value": "",
+                      "textContent": 'Javascript Library 1',
+                      "iconUrl": 'http://www.gravatar.com/avatar.php?gravatar_id=c1eeac01273ee6125e79b2948f184c8b&r=PG&s=16&default=identicon',
+                      "title": 'by Aquilax (28612)',
+                      "updater": "88773",
+                      "rex": [
+                        "^https?:\\/\\/userscripts\\.org\\/scripts\\/source\\/88773\\.user\\.js",
+                        "^https?:\\/\\/userscripts\\.org\\/scripts\\/version\\/\\d+\\/88773\\.user\\.js"
+                      ],
+                      "url": "",
+                      "qs": "",
+                      "securityAdvisory": {
+                        "advisory": "severe",
+                        "title": ", Script homepage is currently missing for peer review"
                       }
                     },
 										"47852": {
@@ -513,7 +530,7 @@
                       "updater": "47852",
                       "rex": [
                         "^https?:\\/\\/userscripts\\.org\\/scripts\\/source\\/47852\\.user\\.js",
-                        "^https?:\\/\\/userscripts\\.org\\/scripts\\/version\\/\d+\\/47852\\.user\\.js"
+                        "^https?:\\/\\/userscripts\\.org\\/scripts\\/version\\/\\d+\\/47852\\.user\\.js"
                       ],
                       "url": "",
                       "qs": "",
