139647413125489f39dae03e602d8d99ff328ec6
Martii <thalamew@hotmail.com>
2011-01-11 23:57:57 -0700

    68219.291539 uso - installWith [0.8.10](/scripts/diff/68219/291539)
    - Added 502 http response code to retry routine too.
    
    Since about 04:00 hours EST the meta.js routine hasn't been returning the metadata block at all... so this will effectively render all standard updaters useless till it's back up.  Working on getting some improvements based off of this outage. Sorry about the inconvenience. :\
    
    meta.js routine appears to be available again :)
    
    68219.291539
    
    28e063bb1f5d15bc6e3db86c132ad2ae9aafcb60

--- src/uso/installWith/uso - installWith.user.js
+++ src/uso/installWith/uso - installWith.user.js
@@ -7,7 +7,7 @@
 // @copyright     2010+, Marti Martz (http://userscripts.org/users/37004)
 // @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @license       Creative Commons; http://creativecommons.org/licenses/by-nc-nd/3.0/
-// @version       0.8.9
+// @version       0.8.10
 // @icon          http://s3.amazonaws.com/uso_ss/icon/68219/thumb.png
 // @include http://userscripts.org/scripts/*/*
 // @include https://userscripts.org/scripts/*/*
@@ -282,8 +282,9 @@
       method: "GET",
       onload: function(xhr) {
         switch(xhr.status) {
-          case 503:
           case 404:
+          case 502:
+          case 503:
             if (--this.retry > 0)
               setTimeout(GM_xmlhttpRequest, 3000 + Math.round(Math.random() * 5000), this);
             break;
