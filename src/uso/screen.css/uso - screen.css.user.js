5f8265bf3da62d9d812cf274cf15d91819d8b2c1
Martii <thalamew@hotmail.com>
2011-01-16 20:22:03 -0700

    48071.292725 uso - screen.css [0.0.6](/scripts/diff/48071/292725)
    - New search form is misaligned with and without alternate CSS script... compensating.
    
    48071.292725
    
    SHA1:adf10f63dd818007790fe71126dd3478353e6a9f

--- src/uso/screen.css/uso - screen.css.user.js
+++ src/uso/screen.css/uso - screen.css.user.js
@@ -4,7 +4,7 @@
 // @description   Corrects any discovered issues with screen.css that come into my scope until uso gets around to fixing and sometimes it will do nothing.
 // @copyright     2009+, Marti Martz (http://userscripts.org/users/37004)
 // @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
-// @version       0.0.5
+// @version       0.0.6
 // @include       http://userscripts.org/*
 //
 // @require       http://usocheckup.redirectme.net/48071.js
@@ -42,6 +42,16 @@
         thisNode.setAttribute("src", url);
       }
     }
+  }
+
+  // New search form metrics
+  var q = document.getElementsByName("q");
+  for each (var name in q) {
+    if (name.getAttribute("size") == 31)
+      name.setAttribute("size", "21");
+
+    GM_addStyle("#header #script_search { margin-top: -6px; padding: 0; }");
+    GM_addStyle('input.text, input.title, input[type="text"] { padding: 3px; }');
   }
 
 })();
