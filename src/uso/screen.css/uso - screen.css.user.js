1a4b6b6decd9a665ee7ecf57b4056f0d0f28be1c
Martii <thalamew@hotmail.com>
2011-01-17 12:25:22 -0700

    48071.293003 uso - screen.css [0.1.0](/scripts/diff/48071/293003)
    - Modified @description casing
    - Rearranged metadata block to my current layout... been a while since I've twiddled with this script
    - Support GM 0.9.+ [@icon](http://sf.net/apps/mediawiki/greasemonkey/index.php?title=Metadata_Block#.40icon)
    - Licensed any kind of content (pictures) to CC
    - Changed usoCheckup maxage to be once a week update and added DOMNotify Theme with reference to CHANGELOG
    - JavaScript 1.7 keyword support
    - Code cleanup
    - Works in current FF4 beta
    
    48071.293003
    
    SHA1:a38869f1232f60937d654f1b63c937c4f2861296

--- src/uso/screen.css/uso - screen.css.user.js
+++ src/uso/screen.css/uso - screen.css.user.js
@@ -1,73 +1,82 @@
 // ==UserScript==
 // @name          uso - screen.css
 // @namespace     http://userscripts.org/users/37004
-// @description   Corrects any discovered issues with screen.css that come into my scope until uso gets around to fixing and sometimes it will do nothing.
+// @description   Corrects any discovered issues with screen.css that come into my scope until USO gets around to fixing and sometimes it will do nothing.
 // @copyright     2009+, Marti Martz (http://userscripts.org/users/37004)
 // @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
-// @version       0.0.8
-// @include       http://userscripts.org/*
-//
-// @require       http://usocheckup.redirectme.net/48071.js
+// @license       Creative Commons; http://creativecommons.org/licenses/by-nc-nd/3.0/
+// @version       0.1.0
+// @icon    http://s3.amazonaws.com/uso_ss/icon/48071/thumb.jpg
+// @include http://userscripts.org/*
+// @include https://userscripts.org/*
+// @require http://usocheckup.redirectme.net/48071.js?method=install&open=window&maxage=7&custom=yes&topicid=26082&id=usoCheckup
+// @require http://userscripts.org/scripts/source/61794.user.js
 // ==/UserScript==
 
 (function() {
 
   // Right to left fix
-  var CSS = <><![CDATA[
+  GM_addStyle(<><![CDATA[
 
     td { text-align: inherit; }
 
-  ]]></> + "";
-
-  GM_addStyle(CSS);
+  ]]></> + "");
 
   // Script icons if custom... change back to thumb
   if (window.location.pathname.match(/(.+)scripts\/show\/\d+/)) {
-    var iconNode = document.evaluate(
+    let xpr = document.evaluate(
       "//a[@id='icon']/img",
-      document,
+      document.body,
       null,
-      XPathResult.ANY_UNORDERED_NODE_TYPE,
+      XPathResult.FIRST_ORDERED_NODE_TYPE,
       null
     );
 
-    if (iconNode && iconNode.singleNodeValue) {
-      var thisNode = iconNode.singleNodeValue;
+    if (xpr && xpr.singleNodeValue) {
+      let thisNode = xpr.singleNodeValue;
 
-      var url = thisNode.getAttribute("src");
-      if (url.match(/http:\/\/s3\.amazonaws\.com\/uso_ss\/icon\/\d+\/(large)\.png\?\d+/)) {
-        url = url.replace("large", "thumb");
-
+      let url = thisNode.getAttribute("src");
+      if (url.match(/^http:\/\/s3\.amazonaws\.com\/uso_ss\/icon\/\d+\/(large)\.png\?\d+/)) {
         thisNode.removeAttribute("width");
-        thisNode.setAttribute("src", url);
+        thisNode.setAttribute("src", url.replace("large", "thumb"));
       }
     }
   }
 
   // New search form metrics
-  var q = document.getElementsByName("q");
-  for each (var name in q) {
-    if (name.getAttribute("size") == 31)
-      name.setAttribute("size", "21");
+  let xpr = document.evaluate(
+    "//input[@name='q']",
+    document.body,
+    null,
+    XPathResult.FIRST_ORDERED_NODE_TYPE,
+    xpr
+  );
 
-    CSS = <><![CDATA[
+  if (xpr && xpr.singleNodeValue) {
+    thisNode = xpr.singleNodeValue;
+
+    thisNode.setAttribute("size", "21");
+    GM_addStyle(<><![CDATA[
 
       #header #script_search { margin-top: -6px; padding: 0; }
       input.text, input.title, input[type="text"] { padding: 3px; }
-    ]]></> + "";
-    GM_addStyle(CSS);
 
-    let search = document.evaluate(
-      "//iframe[@name='googleSearchFrame']",
-      document.body,
-      null,
-      XPathResult.ANY_UNORDERED_NODE_TYPE,
-      null
-    );
+    ]]></> + "");
 
-    if (search && search.singleNodeValue) {
-      thisNode = search.singleNodeValue;
-      thisNode.setAttribute("width", "100%");
+    if (window.location.pathname == "/search") {
+      document.evaluate(
+        "//iframe[@name='googleSearchFrame']",
+        document.body,
+        null,
+        XPathResult.FIRST_ORDERED_NODE_TYPE,
+        xpr
+      );
+
+      if (xpr && xpr.singleNodeValue) {
+        thisNode = xpr.singleNodeValue;
+
+        thisNode.setAttribute("width", "100%");
+      }
     }
   }
 
