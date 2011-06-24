caa4d179e8760c1966145590365f00d2630db7f0
Martii <thalamew@hotmail.com>
2011-06-23 19:22:19 -0600

    105402.345039 uso - Dashboard Quick Menu [0.0.0](/scripts/diff/105402/345039)
    - Initial metadata block commit
    
    105402.345039
    
    SHA1:04f64e212bea7b305918d367b34c02d75f074f46

---
+++ src/uso/Dashboard Quick Menu/uso - Dashboard Quick Menu.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name          uso - Dashboard Quick Menu
+// @namespace     http://userscripts.org/users/37004
+// @description   Enables user dropdown menu
+// @copyright     2011+, Marti Martz (http://userscripts.org/users/37004)
+// @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
+// @license       Creative Commons; http://creativecommons.org/licenses/by-nc-nd/3.0/
+// @icon          http://www.gravatar.com/avatar.php?gravatar_id=e615596ec6d7191ab628a1f0cec0006d&r=PG&s=48&default=identicon
+// @version       0.0.0
+// @include http://userscripts.org/*
+// @include https://userscripts.org/*
+// ==/UserScript==
