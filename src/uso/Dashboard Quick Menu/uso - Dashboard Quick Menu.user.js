79281a68a1a01696202a74e81f690e10e056e0ca
Martii <thalamew@hotmail.com>
2011-06-28 15:00:13 -0600

    105402.346526 uso - Dashboard Quick Menu [0.0.5](/scripts/diff/105402/346526)
    - Added comment link that points to CHANGELOG to fulfill a request. This is currently redundant since usoCheckup already has this but when GM gets updating eventually it will provide additional identification of script. :)
    
    105402.346526
    
    SHA1:bebe13b73a22ad13111087a3dc9567e5a1e76372

--- src/uso/Dashboard Quick Menu/uso - Dashboard Quick Menu.user.js
+++ src/uso/Dashboard Quick Menu/uso - Dashboard Quick Menu.user.js
@@ -8,12 +8,20 @@
 // @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @license       Creative Commons; http://creativecommons.org/licenses/by-nc-nd/3.0/
 // @icon          http://www.gravatar.com/avatar.php?gravatar_id=e615596ec6d7191ab628a1f0cec0006d&r=PG&s=48&default=identicon
-// @version       0.0.4
+// @version       0.0.5
 // @include http://userscripts.org/*
 // @include https://userscripts.org/*
 // @require http://usocheckup.redirectme.net/105402.js?method=install&open=window&maxage=1&custom=yes&topicid=77715&id=usoCheckup
 // @require http://userscripts.org/scripts/source/61794.user.js
 // ==/UserScript==
+
+/*
+
+CHANGELOG
+=========
+http://userscripts.org/topics/77715
+
+*/
 
   // Common function
   function addClass(thisNode, thisValue) {
