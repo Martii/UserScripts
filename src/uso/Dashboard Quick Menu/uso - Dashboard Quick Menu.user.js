bfe4fcc2e4ddd6eb931d25ce867168e2c4886d77
Martii <thalamew@hotmail.com>
2011-07-19 14:34:32 -0600

    105402.353544 uso - Dashboard Quick Menu [-.-.-](/scripts/diff/105402/353544)
    - Updated [@icon](http://sf.net/apps/mediawiki/greasemonkey/index.php?title=Metadata_Block#.40icon) to reflect USO icon addition :) Many thanks Sir Jesse!
    
    105402.353544
    
    SHA1:16ec808d3923797fb0bf9e7956c0891c97cb93a9

--- src/uso/Dashboard Quick Menu/uso - Dashboard Quick Menu.user.js
+++ src/uso/Dashboard Quick Menu/uso - Dashboard Quick Menu.user.js
@@ -7,7 +7,7 @@
 // @copyright     2011+, Marti Martz (http://userscripts.org/users/37004)
 // @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @license       Creative Commons; http://creativecommons.org/licenses/by-nc-nd/3.0/
-// @icon          http://www.gravatar.com/avatar.php?gravatar_id=e615596ec6d7191ab628a1f0cec0006d&r=PG&s=48&default=identicon
+// @icon          http://s3.amazonaws.com/uso_ss/icon/105402/large.png
 // @version       0.1.1
 // @include http://userscripts.org/*
 // @include https://userscripts.org/*
