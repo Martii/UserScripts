4bd8adf1cc9abc78427d049e08cf33b9143258b8
Martii <thalamew@hotmail.com>
2011-09-29 21:29:42 -0600

    48200.380341 uso - Cancellable Editor [0.0.16](/scripts/diff/48200/380341)
    - Update found when visiting AOM... less than 24 hours (~23 hours) but closer this time.
    - Triggering next update test (~11:30PM EST)
    
    48200.380341
    
    SHA1:0d5d58aefc96b25e2c7a6dc6aaf247a6e643d51f

--- src/uso/Cancellable Editor/uso - Cancellable Editor.user.js
+++ src/uso/Cancellable Editor/uso - Cancellable Editor.user.js
@@ -8,7 +8,7 @@
 // @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @license       (CC) Attribution Non-Commercial Share Alike; http://creativecommons.org/licenses/by-nc-sa/3.0/
 // @icon          http://s3.amazonaws.com/uso_ss/icon/48200/large.png
-// @version       0.0.15
+// @version       0.0.16
 //
 // @include  http://userscripts.org/topics/*
 // @include  https://userscripts.org/topics/*
