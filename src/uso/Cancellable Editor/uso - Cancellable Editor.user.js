cacd16da1d0249a615d11a8b8f65561d7ef4a8cc
Martii <thalamew@hotmail.com>
2011-10-07 07:13:42 -0600

    48200.386302 uso - Cancellable Editor [0.0.19](/scripts/diff/48200/386302) - remove @icon
    48200.386302
    
    SHA1:403688d0f714d4678078d062927857a64dfda912

--- src/uso/Cancellable Editor/uso - Cancellable Editor.user.js
+++ src/uso/Cancellable Editor/uso - Cancellable Editor.user.js
@@ -7,7 +7,8 @@
 // @copyright     2009+, Marti Martz (http://userscripts.org/users/37004)
 // @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @license       (CC) Attribution Non-Commercial Share Alike; http://creativecommons.org/licenses/by-nc-sa/3.0/
-// @version       0.0.18
+//@icon          http://s3.amazonaws.com/uso_ss/icon/48200/large.png
+// @version       0.0.19
 //
 // @include  http://userscripts.org/topics/*
 // @include  https://userscripts.org/topics/*
