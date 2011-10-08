6a25d3223b1e680757dc547215e7d05022916e84
Martii <thalamew@hotmail.com>
2011-10-07 22:22:58 -0600

    114843.386580 uso - Monkey Barrel [0.0.3](/scripts/diff/114843/386580)
    - Missed removal of less than...typo
    
    114843.386580
    
    SHA1:7b5779115b7f26a4b217fb7fea736ea1d268a39f

--- src/uso/Monkey Barrel/uso - Monkey Barrel.user.js
+++ src/uso/Monkey Barrel/uso - Monkey Barrel.user.js
@@ -8,7 +8,7 @@
 // @copyright     2011+, Marti Martz (http://userscripts.org/users/37004)
 // @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @license       (CC); http://creativecommons.org/licenses/by-nc-sa/3.0/
-// @version       0.0.2
+// @version       0.0.3
 // @icon          http://s3.amazonaws.com/uso_ss/icon/114843/large.png
 //
 // @include   /https?:\/\/userscripts\.org\/.*/
@@ -179,7 +179,7 @@ Please note this script uses native JSON and native classList which requires Fir
             },
             'importGroups': {
                 "type": 'checkbox',
-                "label": 'Enable automatic import for subscribed Groups when clicked<',
+                "label": 'Enable automatic import for subscribed Groups when clicked',
                 "default": true
             }
           }
