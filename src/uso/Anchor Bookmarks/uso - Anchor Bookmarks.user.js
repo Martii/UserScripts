04bde8cd0182e15ef196283e0573a526078cb15e
Martii <thalamew@hotmail.com>
2011-10-10 18:28:11 -0600

    69725.387891 uso - Anchor Bookmarks [0.5.2](/scripts/diff/69725/387891)
    - Remove some exclusions as GM doesn't inject on them
    
    69725.387891
    
    SHA1:8f2a465997ee3d0c2e95a6649dd32a158fa6e3c4

--- src/uso/Anchor Bookmarks/uso - Anchor Bookmarks.user.js
+++ src/uso/Anchor Bookmarks/uso - Anchor Bookmarks.user.js
@@ -9,15 +9,13 @@
 // @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @license       Creative Commons; http://creativecommons.org/licenses/by-nc-nd/3.0/
 // @icon          http://www.gravatar.com/avatar.php?gravatar_id=e615596ec6d7191ab628a1f0cec0006d&r=PG&s=48&default=identicon
-// @version       0.5.1
+// @version       0.5.2
 //
 // @include http://userscripts.org/*
 // @include https://userscripts.org/*
 //
 // @exclude http://userscripts.org/login*
 // @exclude https://userscripts.org/login*
-// @exclude http://userscripts.org/scripts/source/*.meta.js
-// @exclude https://userscripts.org/scripts/source/*.meta.js
 // @exclude http://userscripts.org/scripts/diff/*
 // @exclude https://userscripts.org/scripts/diff/*
 // @exclude http://userscripts.org/scripts/version/*
