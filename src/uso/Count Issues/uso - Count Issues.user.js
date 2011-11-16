41cb6d29bcdeaf921502d24ff51f848004d7b67f
Martii <thalamew@hotmail.com>
2011-11-15 20:44:24 -0700

    69307.401772 uso - Count Issues [0.16.3](/scripts/diff/69307/401772)
    - Sync with backend opts
    
    69307.401772
    
    0334171721cac56d241be9aa29020a0f0358aaab

--- src/uso/Count Issues/uso - Count Issues.user.js
+++ src/uso/Count Issues/uso - Count Issues.user.js
@@ -8,7 +8,7 @@
 // @contributor   sizzlemctwizzle (http://userscripts.org/users/27715)
 // @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @license       Creative Commons; http://creativecommons.org/licenses/by-nc-nd/3.0/
-// @version       0.16.2
+// @version       0.16.3
 // @icon          https://s3.amazonaws.com/uso_ss/icon/69307/large.png
 //
 // @include   http://userscripts.org/scripts/*/*
