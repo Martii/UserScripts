6dcd9575efde2ccf5adc5f78ce99d75bf5adfcd8
Martii <thalamew@hotmail.com>
2012-11-21 05:23:05 -0700

    69725.527497 uso - Anchor Bookmarks [0.5.4](/scripts/diff/69725/527497)
    - Changed dependency to SSL state
    
    69725.527497
    
    SHA1:4178494c8644842bc8f9b9dbbf7a655a163bb01f

--- src/uso/Anchor Bookmarks/uso - Anchor Bookmarks.user.js
+++ src/uso/Anchor Bookmarks/uso - Anchor Bookmarks.user.js
@@ -8,8 +8,8 @@
 // @contributor   sizzlemctwizzle (http://userscripts.org/users/27715)
 // @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @license       Creative Commons; http://creativecommons.org/licenses/by-nc-nd/3.0/
-// @icon          http://www.gravatar.com/avatar.php?gravatar_id=e615596ec6d7191ab628a1f0cec0006d&r=PG&s=48&default=identicon
-// @version       0.5.3
+// @version       0.5.4
+// @icon          https://www.gravatar.com/avatar.php?gravatar_id=e615596ec6d7191ab628a1f0cec0006d&r=PG&s=48&default=identicon
 //
 // @include http://userscripts.org/*
 // @include https://userscripts.org/*
