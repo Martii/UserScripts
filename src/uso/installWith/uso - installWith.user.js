c9e7a191776f88941e22fed556470a2b0e982ad1
Martii <thalamew@hotmail.com>
2012-11-23 10:24:46 -0700

    68219.528013 uso - installWith [1.1.4](/scripts/diff/68219/528013)
    - Fix for rex on [this scripts](/scripts/show/10448) detection.
    
    68219.528013
    
    bddf1675d66fd54a65fece2a427489fcf5815f56

--- src/uso/installWith/uso - installWith.user.js
+++ src/uso/installWith/uso - installWith.user.js
@@ -7,7 +7,7 @@
 // @copyright     2010+, Marti Martz (http://userscripts.org/users/37004)
 // @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @license       Creative Commons; http://creativecommons.org/licenses/by-nc-nd/3.0/
-// @version       1.1.3
+// @version       1.1.4
 // @icon          https://s3.amazonaws.com/uso_ss/icon/68219/large.png
 //
 // @include /^https?:\/\/userscripts\.org\/scripts\/.*/
@@ -1109,7 +1109,7 @@
                 "title": 'by Aquilax (28612)',
                 "rex": [
                   "^https?:\\/\\/userscripts\\.org\\/scripts\\/source\\/33024\\.user\\.js",
-                  "^https?:\\/\\/userscripts\\.org\\/scripts\\/version\\/33024\\/(\\d+)\\.user\\.js"
+                  "^https?:\\/\\/userscripts\\.org\\/scripts\\/version\\/33024\\/(?:\\d+)\\.user\\.js"
                 ],
                 "securityAdvisory": {
                   "advisory": "guarded",
