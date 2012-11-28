6261e9438b16e831cc96201cc623f5fdc7c95ea7
Martii <thalamew@hotmail.com>
2012-11-28 02:50:54 -0700

    48071.529328 uso - screen.css [0.3.0](/scripts/diff/48071/529328)
    - Remove all but [ltr fix](/topics/26082#posts-117336)... seems the remainder of the issues were fixed.
    - Using [GM_setStyle](/scripts/show/115323) alternative to GM_addStyle for better and cleaner DOM.
    - Added GM regular expression [@include](http://sf.net/apps/mediawiki/greasemonkey/index.php?title=Metadata_Block#.40include) for quicker injection response
    - Using SSL for [@icon](http://sf.net/apps/mediawiki/greasemonkey/index.php?title=Metadata_Block#.40icon)
    - Removed updater... please use [uso - installWith](/scripts/show/68219) if you need one back.
    
    NOTE: I won't be using E4X in the future since it has none... e.g. Firefox 17 disabled it for GM scripts and Firefox 18 has it planned to remove the module altogether.
    
    48071.529328
    
    SHA1:80acb6a9118d6c50ac3f4dea92908040d47b8aa2

--- src/uso/screen.css/uso - screen.css.user.js
+++ src/uso/screen.css/uso - screen.css.user.js
@@ -5,105 +5,32 @@
 // @copyright     2009+, Marti Martz (http://userscripts.org/users/37004)
 // @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @license       Creative Commons; http://creativecommons.org/licenses/by-nc-nd/3.0/
-// @version       0.2.2
-// @icon    http://s3.amazonaws.com/uso_ss/icon/48071/thumb.jpg
+// @version       0.3.0
+// @icon          https://s3.amazonaws.com/uso_ss/icon/48071/large.jpg
+//
+// @include /https?:\/\/userscripts\.org\/.*/
 //
 // @include http://userscripts.org/*
 // @include https://userscripts.org/*
-// @require http://usocheckup.redirectme.net/48071.js?method=install&open=window&maxage=7&custom=yes&topicid=26082&id=usoCheckup
-// @require http://userscripts.org/scripts/source/61794.user.js
+//
+// @require https://userscripts.org/scripts/source/115323.user.js
+//
 // ==/UserScript==
 
 (function() {
 
+  let gCSS = GM_setStyle({
+    media: "screen, projection, print"
+  });
+
   // Right to left fix
-  GM_addStyle(<><![CDATA[
+  GM_setStyle({
+    node: gCSS,
+    data:
+      [
+        "td { text-align: inherit; }"
 
-    td { text-align: inherit; }
-
-  ]]></> + "");
-
-  // Script icons if custom... change back to thumb
-  let xpr = document.evaluate(
-    "//a[@id='icon']/img",
-    document.body,
-    null,
-    XPathResult.FIRST_ORDERED_NODE_TYPE,
-    null
-  );
-
-  if (xpr && xpr.singleNodeValue) {
-    let thisNode = xpr.singleNodeValue;
-
-    let url = thisNode.getAttribute("src");
-    if (url.match(/^http:\/\/s3\.amazonaws\.com\/uso_ss\/icon\/\d+\/(large)\.png\?\d+/)) {
-      //thisNode.removeAttribute("width");
-      thisNode.setAttribute("src", url.replace("large", "thumb"));
-    }
-  }
-
-  // New search form metrics
-  let xpr = document.evaluate(
-    "//input[@name='q']",
-    document.body,
-    null,
-    XPathResult.FIRST_ORDERED_NODE_TYPE,
-    null
-  );
-
-  if (xpr && xpr.singleNodeValue) {
-    thisNode = xpr.singleNodeValue;
-
-    thisNode.setAttribute("size", "20");
-
-    if (window.location.pathname == "/search") {
-      document.evaluate(
-        "//iframe[@name='googleSearchFrame']",
-        document.body,
-        null,
-        XPathResult.FIRST_ORDERED_NODE_TYPE,
-        xpr
-      );
-
-      if (xpr && xpr.singleNodeValue) {
-        thisNode = xpr.singleNodeValue;
-
-        thisNode.setAttribute("width", "100%");
-      }
-    }
-  }
-
-  // Play nice with Better Search Preferences
-  function resizeOnceSearch() {
-    let xpr = document.evaluate(
-      "//a[@title='Better Search Preferences']",
-      document.body,
-      null,
-      XPathResult.FIRST_ORDERED_NODE_TYPE,
-      null
-    );
-
-    if (xpr && xpr.singleNodeValue) {
-      document.evaluate(
-        "//input[@name='q']",
-        document.body,
-        null,
-        XPathResult.FIRST_ORDERED_NODE_TYPE,
-        xpr
-      );
-
-      if (xpr && xpr.singleNodeValue) {
-        let thisNode = xpr.singleNodeValue;
-
-        thisNode.setAttribute("size", "24");
-      }
-    }
-  }
-  window.setTimeout(resizeOnceSearch, 50); // Increase delay if not resizing
-
-  // Soften coloring on certain menu drop downs
-  GM_addStyle(<><![CDATA[
-    #header #mainmenu li div a, #header #mainmenu li.active div a, #header #mainmenu li.active div a:hover { background-color: #000; color: #fff; }
-  ]]></> + "");
+      ].join("\n")
+  });
 
 })();
