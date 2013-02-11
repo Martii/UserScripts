5abcbdeab11369d0efb7ebccda031bb66ffd0b6f
Martii <thalamew@hotmail.com>
2013-02-11 15:19:20 -0700

    158922.552912 uso - Searchin' [0.0.0](/scripts/diff/158922/552912)
    - Initial generic metadata block commit
    - This scriptid is reserved until I can modify some old source on a project that will be posted eventually and I need the scriptid to configure.
    
    158922.552912
    
    SHA1:e8aff50fdaea9ca94c769f6b913647ec41e82b2f

---
+++ src/uso/Searchin'/uso - Searchin'.user.js
@@ -0,0 +1,19 @@
+(function () {
+
+// ==UserScript==
+// @name          Scriptid Reservation
+// @namespace     http://userscripts.org/users/37004
+// @description   Reserving a scriptid for a future publication
+// @copyright     2011+, Marti Martz (http://userscripts.org/users/37004)
+// @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
+// @license       Creative Commons; http://creativecommons.org/licenses/by-nc-nd/3.0/
+// @version       0.0.0
+// @icon          https://secure.gravatar.com/avatar/e615596ec6d7191ab628a1f0cec0006d?r=PG&s=48&default=identicon
+//
+// @exclude       *
+//
+// ==/UserScript==
+
+  alert('This is a reserved scriptid');
+
+})();
