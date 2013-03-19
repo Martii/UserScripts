8b0d9ac38d87e3e522cb3d5fb9306c3f351db2bc
Martii <thalamew@hotmail.com>
2013-03-18 21:44:18 -0600

    162319.567396 uso - Retry [0.0.2](/scripts/diff/162319/567396)
    - Fix max out to be more accurate
    - Whitespace standardize
    - Include on SSL
    
    162319.567396
    
    SHA1:5053ebb852c09c48ebdabc8e37f6019868ccbc3a

--- src/uso/Retry/uso - Retry.user.js
+++ src/uso/Retry/uso - Retry.user.js
@@ -8,11 +8,12 @@
 // @copyright     2011+, Marti Martz (http://userscripts.org/users/37004)
 // @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @license       Creative Commons; http://creativecommons.org/licenses/by-nc-nd/3.0/
-// @version       0.0.1
+// @version       0.0.2
 // @include  http://userscripts.org/*
+// @include  https://userscripts.org/*
 // ==/UserScript==
 
-  function retry () {
+  function retry() {
     let
       min = 750,
       max = 5000
@@ -21,15 +22,15 @@
     if (location.pathname.match(/^\/sessions/))
       setTimeout(function () {
         location.pathname = "/login";
-      }, min + Math.round(Math.random() * max));
+      }, min + Math.round(Math.random() * (max - min)));
     else
       setTimeout(function () {
         location.reload();
-      }, min + Math.round(Math.random() * max));
+      }, min + Math.round(Math.random() * (max - min)));
 
   }
 
-  function retryCheck () {
+  function retryCheck() {
     if (document.body) {
       let xpr = document.evaluate(
         "//title[.='502 Bad Gateway' or .='503 Service Unavailable' or .='404 Not Found']",
