fad3164e7cc4f655f84910f7de5152e50dfe188e
Martii <thalamew@hotmail.com>
2013-07-01 17:48:50 -0600

    162319.614678 uso - Retry [0.0.11](/scripts/diff/162319/614678)
    - Removed one missed var declaration in favor of let.
    
    Sorry about the extra noise.
    
    Not a huge difference between tabs and spaces.
    
    RESULTS:
    meta.js byte count is smaller... expectedmeta.js whitespace is 4.22% smaller... not much differenceuser.js byte count is smaller... expecteduser.js whitespace is 11% smaller... again not much of a difference
    
    162319.614678
    
    SHA1:76bfe499bb9e25d0d95f31e3b8e3f207993872d5

--- src/uso/Retry/uso - Retry.user.js
+++ src/uso/Retry/uso - Retry.user.js
@@ -8,13 +8,13 @@
 // @copyright	2011+, Marti Martz (http://userscripts.org/users/37004)
 // @license	GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @license	Creative Commons; http://creativecommons.org/licenses/by-nc-nd/3.0/
-// @version	0.0.10
+// @version	0.0.11
 // @include	/^https?://userscripts\.org/?/
 // @include	http://userscripts.org/*
 // @include	https://userscripts.org/*
 // ==/UserScript==
 
-	var
+	let
 			MutationObserver = window.MutationObserver || window.WebKitMutationObserver,
 			min = 750,
 			max = 5000,
