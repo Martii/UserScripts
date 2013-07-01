f15a7361ed230a073d0e3787685e07fe2558d930
Martii <thalamew@hotmail.com>
2013-07-01 17:44:05 -0600

    162319.614675 uso - Retry [0.0.9](/scripts/diff/162319/614675)
    - Additional head tag check
    - Check for MutationObserver existence. Leaving cross-browser prefixed name in just on the rare chance that the other browsers might implement everything all in one shot... but unlikely since it's been over five (5) years waiting.
    - Simplified re include
    - Clean up MutationObserver... not going to check attributes since it seems to be working okay without.
    - Trying out tabs instead of spaces in this script just to see decrease percentage and how USO handles it. Makes it harder to read online but Userscripts.org Alternate CSS can fix that. :)
    
    NOTES:
    
    Some SeaMonkey versions don't have the console object completed UGHH! So version 0.0.2 is recommended for those. Try the most recent first though.Windows Safari and Opera don't have MutationObserver object at all.Goo of course isn't ECMAScript 6.x compliant so the let statements will cause failure. Goo native also is having severe issues with @run-at document-start and TamperMonkey doesn't seem to support it either.
    
    Tried to make this one cross-browser but again too many failures in the other browsers at this time. :\
    
    162319.614675
    
    SHA1:d8c40ac8f542938405ad70db9f210f2ab5f0fd65

--- src/uso/Retry/uso - Retry.user.js
+++ src/uso/Retry/uso - Retry.user.js
@@ -1,79 +1,81 @@
 (function () {
 
 // ==UserScript==
-// @run-at        document-start
-// @name          uso - Retry
-// @namespace     http://userscripts.org/users/37004
-// @description   Auto-Refreshes current URI on USO when a non-200 response code is encountered.
-// @copyright     2011+, Marti Martz (http://userscripts.org/users/37004)
-// @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
-// @license       Creative Commons; http://creativecommons.org/licenses/by-nc-nd/3.0/
-// @version        0.0.8
-// @include  /^https?:\/\/userscripts\.org\/.*/
-// @include  http://userscripts.org/*
-// @include  https://userscripts.org/*
+// @run-at	document-start
+// @name	uso - Retry
+// @namespace	http://userscripts.org/users/37004
+// @description	Auto-Refreshes current URI on USO when a non-200 response code is encountered.
+// @copyright	2011+, Marti Martz (http://userscripts.org/users/37004)
+// @license	GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
+// @license	Creative Commons; http://creativecommons.org/licenses/by-nc-nd/3.0/
+// @version	0.0.9
+// @include	/^https?://userscripts\.org/?/
+// @include	http://userscripts.org/*
+// @include	https://userscripts.org/*
+// @grant	GM_log
 // ==/UserScript==
 
-  let
-      min = 750,
-      max = 5000,
-      timeout = min + Math.round(Math.random() * (max - min)),
-      blocking = false
-  ;
+	var
+			MutationObserver = window.MutationObserver || window.WebKitMutationObserver,
+			min = 750,
+			max = 5000,
+			timeout = min + Math.round(Math.random() * (max - min)),
+			blocking = false
+	;
 
-  let observer = new MutationObserver(
-    function (aMutations) {
-      aMutations.forEach(
-        function (aMutation) {
-          if (!blocking) {
-            switch (aMutation.type) {
-              case "childList":
-                let xpr = document.evaluate(
-                  "//title",
-                  headNode,
-                  null,
-                  XPathResult.FIRST_ORDERED_NODE_TYPE,
-                  null
-                );
-                if (xpr && xpr.singleNodeValue) {
+	if (MutationObserver) {
+		let observer = new MutationObserver(
+			function (aMutations) {
+				aMutations.forEach(
+					function (aMutation) {
+						if (!blocking) {
+							switch (aMutation.type) {
+								case "childList":
+									let xpr = document.evaluate(
+										"//title",
+										headNode,
+										null,
+										XPathResult.FIRST_ORDERED_NODE_TYPE,
+										null
+									);
+									if (xpr && xpr.singleNodeValue) {
 
-                  blocking = true;
-                  observer.disconnect();
+										blocking = true;
+										observer.disconnect();
 
-                  let thisNode = xpr.singleNodeValue;
+										let thisNode = xpr.singleNodeValue;
 
-                  switch (thisNode.textContent) {
-                    case "502 Bad Gateway":
-                    case "503 Service Unavailable":
-                    case "404 Not Found":
-                      setTimeout(function () {
-                        location.reload();
-                      }, timeout);
+										switch (thisNode.textContent) {
+											case "502 Bad Gateway":
+											case "503 Service Unavailable":
+											case "404 Not Found":
+												setTimeout(function () {
+													location.reload();
+												}, timeout);
 
-                      break;
-                  }
-                }
-                break;
-            }
-          }
-        }
-      );
-    }
-  );
+												break;
+										}
+									}
+									break;
+							}
+						}
+					}
+				);
+			}
+		);
 
+		let headNode = document.head || document.getElementsByTagName("head")[0];
+		if (headNode) {
+			let config = {
+				childList: true
+			};
 
-  let headNode = document.head;
-  if (headNode) {
-
-    let config = {
-      attributes: true,
-      childList: true
-    };
-
-    observer.observe(headNode, config);
-
-  }
-  else
-    console.error("Failed to get headNode");
+			observer.observe(headNode, config);
+		}
+		else
+			console.error("Failed to get headNode");
+	}
+	else
+		console.error("MutationObserver does not exist");
 
 })();
