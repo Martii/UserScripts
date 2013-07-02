51d1ccd1e8b0d857909e2aaa909a739e234fe645
Martii <thalamew@hotmail.com>
2013-07-01 18:59:37 -0600

    162319.614698 uso - Retry [0.1.0](/scripts/diff/162319/614698)
    - Add @icon
    - Some whitespace in metadata block
    
    162319.614698
    
    SHA1:8b53d5733340473f601f899af154673bb3c648aa

--- src/uso/Retry/uso - Retry.user.js
+++ src/uso/Retry/uso - Retry.user.js
@@ -9,10 +9,13 @@
 // @copyright   2011+, Marti Martz (http://userscripts.org/users/37004)
 // @license     GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @license     Creative Commons; http://creativecommons.org/licenses/by-nc-nd/3.0/
-// @version     0.0.12
+// @version     0.1.0
+// @icon https://secure.gravatar.com/avatar/e615596ec6d7191ab628a1f0cec0006d?r=PG&s=48&default=identicon
+
 // @include /^https?://userscripts\.org/?/
 // @include http://userscripts.org/*
 // @include https://userscripts.org/*
+
 // ==/UserScript==
 
   let
