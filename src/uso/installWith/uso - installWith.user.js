a26722d921b543a0c559fbea096fb5bb9a55e21c
Martii <thalamew@hotmail.com>
2013-09-30 23:47:37 -0600

    68219.645128 uso - installWith [2.0.0.20](/scripts/diff/68219/645128)
    - AAU is offline until further notice. Redirected backend for current wrapped installs to use usoC to prevent breakage on updates.
    - Definitions update
    
    68219.645128
    
    a2e5f88faa617962cbbedf1ac86807b10d6041a3

--- src/uso/installWith/uso - installWith.user.js
+++ src/uso/installWith/uso - installWith.user.js
@@ -8,7 +8,7 @@
 // @copyright     2010+, Marti Martz (http://userscripts.org/users/37004)
 // @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @license       Creative Commons; http://creativecommons.org/licenses/by-nc-nd/3.0/
-// @version       2.0.0.19
+// @version       2.0.0.20
 // @icon          https://s3.amazonaws.com/uso_ss/icon/68219/large.png
 
 // @include /^https?://userscripts.org/?$/
