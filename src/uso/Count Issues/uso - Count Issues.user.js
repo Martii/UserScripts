aa7ff39440583c24c25ed9cd9dfa753f2fb7675e
Martii <thalamew@hotmail.com>
2014-02-08 17:31:00 -0700

    69307.886445 uso - Count Issues [1.0.0.8](/scripts/diff/69307/886445)
    - Attempt to disable wrap on re textareas in GMC.
    
    69307.886445
    
    23769bff8511f8a8daac1e6747394cb67dc3cbb2

--- src/uso/Count Issues/uso - Count Issues.user.js
+++ src/uso/Count Issues/uso - Count Issues.user.js
@@ -10,7 +10,7 @@
 // @contributor   sizzlemctwizzle (http://userscripts.org/users/27715)
 // @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @license       Creative Commons; http://creativecommons.org/licenses/by-nc-nd/3.0/
-// @version       1.0.0.7
+// @version       1.0.0.8
 // @icon          https://s3.amazonaws.com/uso_ss/icon/69307/large.png
 
 // @include   /^https?://userscripts\.org/scripts//
@@ -1819,6 +1819,11 @@
       'showVersionsKeysStringHeight': { "type": 'hidden', "default": "16px" }
     }
   );
+
+  gmcHome.onOpen = function () {
+    gmcHome.fields["showStringsStringPre"].node.setAttribute("wrap", "off");
+    gmcHome.fields["showStringsString"].node.setAttribute("wrap", "off");
+  }
 
   gmcHome.onSave = function () {
     let
