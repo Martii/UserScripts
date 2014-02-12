2c24b2c5b9fb1c99ad8947597398e12036b77591
Martii <thalamew@hotmail.com>
2014-02-12 02:06:32 -0700

    378796.910700 uso - Sticky Topics [1.0.4](/scripts/diff/378796/910700)
    - Add in the safe AOU URLs
    
    378796.910700
    
    SHA1:7ecc45b6718d7f52334444a51c440a15d0fb64ce

--- src/uso/Sticky Topics/uso - Sticky Topics.user.js
+++ src/uso/Sticky Topics/uso - Sticky Topics.user.js
@@ -8,11 +8,15 @@
 // @copyright     2014+, Marti Martz (http://userscripts.org/users/37004)
 // @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @license       Creative Commons; http://creativecommons.org/licenses/by-nc-nd/3.0/
-// @version       1.0.3
+// @version       1.0.4
 // @icon          https://www.gravatar.com/avatar/e615596ec6d7191ab628a1f0cec0006d?r=PG&s=48&default=identicon
 
 // @include       http://userscripts.org/scripts/show/*
 // @include       https://userscripts.org/scripts/show/*
+
+// @updateURL   https://userscripts.org/scripts/source/378796.meta.js
+// @installURL  https://userscripts.org/scripts/source/378796.user.js
+// @downloadURL https://userscripts.org/scripts/source/378796.user.js
 
 // @grant  none
 
