39458575f49ea8f631bc5a89b0d2c8509f215bf0
Martii <thalamew@hotmail.com>
2014-02-12 04:03:46 -0700

    378796.911078 uso - Sticky Topics [1.0.6](/scripts/diff/378796/911078)
    - Include regular expression @include
    
    378796.911078
    
    SHA1:747727f334bbe1b13409437f39e038145c9d7636

--- src/uso/Sticky Topics/uso - Sticky Topics.user.js
+++ src/uso/Sticky Topics/uso - Sticky Topics.user.js
@@ -8,8 +8,10 @@
 // @copyright     2014+, Marti Martz (http://userscripts.org/users/37004)
 // @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @license       Creative Commons; http://creativecommons.org/licenses/by-nc-nd/3.0/
-// @version       1.0.5
+// @version       1.0.6
 // @icon          https://www.gravatar.com/avatar/e615596ec6d7191ab628a1f0cec0006d?r=PG&s=48&default=identicon
+
+// @include       /^https?://userscripts\.org/scripts/show/?/
 
 // @include       http://userscripts.org/scripts/show/*
 // @include       https://userscripts.org/scripts/show/*
