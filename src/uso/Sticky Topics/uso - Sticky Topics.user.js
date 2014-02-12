53c5b0d136ab80a51ac4a9b39948d2abf5e6178d
Martii <thalamew@hotmail.com>
2014-02-11 20:30:35 -0700

    378796.909618 uso - Sticky Topics [1.0.0](/scripts/diff/378796/909618)
    - Initial main source commit
    
    378796.909618
    
    SHA1:d129d691c366c535c420b168657db2adf9b88cb2

--- src/uso/Sticky Topics/uso - Sticky Topics.user.js
+++ src/uso/Sticky Topics/uso - Sticky Topics.user.js
@@ -8,7 +8,7 @@
 // @copyright     2014+, Marti Martz (http://userscripts.org/users/37004)
 // @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @license       Creative Commons; http://creativecommons.org/licenses/by-nc-nd/3.0/
-// @version       0.0.0
+// @version       1.0.0
 // @icon          https://s3.amazonaws.com/uso_ss/icon/68219/large.png
 
 // @include       http://userscripts.org/scripts/show/*
@@ -18,5 +18,116 @@
 
 // ==/UserScript==
 
+  /**
+   *
+   */
+  var abort = false;
 
+  var full_descriptionNode = document.getElementById("full_description");
+  if (full_descriptionNode) {
+
+    // Validate table
+    var tableNodes = document.querySelectorAll("table");
+    for (var i = 0, tableNode; tableNode = tableNodes[i++];) {
+
+      var thNodes = tableNode.querySelectorAll("th");
+      if (thNodes && thNodes.length == 2 && thNodes[0].textContent == "Sticky Topics" && thNodes[1].textContent == "Posts") {
+
+
+        var trNodes = tableNode.querySelectorAll("tr");
+        if (trNodes) {
+          for (var j = 0, trNode; trNode = trNodes[j++];) {
+            var tid = null;
+
+            var tdNodes = trNode.querySelectorAll("td");
+            for (var k = 0, tdNode; tdNode = tdNodes[k]; k++) {
+              switch (k) {
+                case 0:
+                  var aNodes = tdNode.querySelectorAll("a");
+                  if (aNodes && aNodes.length == 1) {
+                    for (var l = 0, aNode; aNode = aNodes[l++];) {
+                      var matches = aNode.href.match(/^(?:https?:\/\/userscripts\.org)\/topics\/(\d+)/i);
+                      if (matches) {
+                        tid = matches[1];
+                        aNode.href = "/topics/" + tid + aNode.hash;
+
+                        var textContent = aNode.textContent;
+                        if (textContent.length > 30)
+                          aNode.textContent = textContent.substr(0, 27) + "...";
+                      }
+                      else
+                        abort = true;
+                    }
+                  }
+                  else
+                    abort = true;
+                  break;
+
+                case 1:
+                  var aNodes = tdNode.querySelectorAll("a");
+                  if (aNodes && aNodes.length > 0) {
+                    if (aNodes.length == 1) {
+                      aNode = aNodes[0];
+
+                      aNode.href = "/topics/" + tid + "#posts-last";
+                      aNode.textContent = "\u00BB";
+                    }
+                    else
+                      abort = true;
+                  }
+                  else {
+                    tdNode.textContent = "\u2013";
+                  }
+
+                  break;
+                default:
+                  abort = true;
+              }
+
+            }
+
+            if (!abort) {
+              /**
+              * Add styling
+              */
+              var css = [
+                  '#right #stickytopics { font-size: 0.9em; }',
+                  '#right #stickytopics td:last-child { text-align: right; }'
+
+              ].join('');
+
+              var nodeStyle = document.createElement("style");
+              nodeStyle.type = "text/css";
+              nodeStyle.textContent = css;
+
+              document.head.appendChild(nodeStyle);
+
+              /**
+              * Symmetry with USO
+              */
+              tableNode.width = "100%";
+
+
+              /**
+              * Move the table
+              */
+              var nodeDiv = document.createElement("div");
+              nodeDiv.id = "stickytopics";
+
+              nodeDiv.appendChild(tableNode);
+
+              var topicsNode = document.getElementById("topics");
+              var script_sidebarNode = document.getElementById("script_sidebar");
+              if (script_sidebarNode && topicsNode) {
+                script_sidebarNode.insertBefore(nodeDiv, topicsNode);
+              }
+
+              abort = false;
+            }
+
+          }
+        }
+      }
+    }
+  }
 })();
