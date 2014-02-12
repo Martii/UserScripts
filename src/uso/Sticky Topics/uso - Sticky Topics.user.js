c0602d7afaba7a2ad147e3b5ae9ec43b8f5a7f1f
Martii <thalamew@hotmail.com>
2014-02-11 20:27:38 -0700

    378796.909610 uso - Sticky Topics [0.0.0](/scripts/diff/378796/909610)
    - Initial metadata commit with IIFE
    
    378796.909610
    
    SHA1:e20697d8d8ab7172748586f956d324a8b8ca805e

---
+++ src/uso/Sticky Topics/uso - Sticky Topics.user.js
@@ -0,0 +1,22 @@
+(function () {
+  "use strict";
+
+// ==UserScript==
+// @name          uso - Sticky Topics
+// @namespace     http://userscripts.org/users/37004
+// @description   Moves a formatted table from the main script homepage description area to the Forum Activity sidebar header
+// @copyright     2014+, Marti Martz (http://userscripts.org/users/37004)
+// @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
+// @license       Creative Commons; http://creativecommons.org/licenses/by-nc-nd/3.0/
+// @version       0.0.0
+// @icon          https://s3.amazonaws.com/uso_ss/icon/68219/large.png
+
+// @include       http://userscripts.org/scripts/show/*
+// @include       https://userscripts.org/scripts/show/*
+
+// @grant  none
+
+// ==/UserScript==
+
+
+})();
