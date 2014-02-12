8953e9f7f20d03bcafc190346a92139c68c3c627
Martii <thalamew@hotmail.com>
2014-02-12 00:03:43 -0700

    378796.910301 uso - Sticky Topics [1.0.3](/scripts/diff/378796/910301)
    - Simplify this a bit. Should now work in nested tables. :)
    
    378796.910301
    
    SHA1:936636b009013462a6d35596eccc9f5f07e1215f

--- src/uso/Sticky Topics/uso - Sticky Topics.user.js
+++ src/uso/Sticky Topics/uso - Sticky Topics.user.js
@@ -8,7 +8,7 @@
 // @copyright     2014+, Marti Martz (http://userscripts.org/users/37004)
 // @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @license       Creative Commons; http://creativecommons.org/licenses/by-nc-nd/3.0/
-// @version       1.0.2
+// @version       1.0.3
 // @icon          https://www.gravatar.com/avatar/e615596ec6d7191ab628a1f0cec0006d?r=PG&s=48&default=identicon
 
 // @include       http://userscripts.org/scripts/show/*
@@ -21,113 +21,113 @@
   /**
    *
    */
-  var abort = false;
+
+  function moveTable(aTable) {
+    /**
+    * Add styling
+    */
+    var css = [
+        '#right #stickytopics { font-size: 0.9em; }',
+        '#right #stickytopics td:last-child { text-align: right; }'
+
+    ].join('');
+
+    var nodeStyle = document.createElement("style");
+    nodeStyle.type = "text/css";
+    nodeStyle.textContent = css;
+
+    document.head.appendChild(nodeStyle);
+
+    /**
+    * Symmetry with USO
+    */
+    tableNode.width = "100%";
+
+
+    /**
+    * Move the table
+    */
+    var nodeDiv = document.createElement("div");
+    nodeDiv.id = "stickytopics";
+
+    nodeDiv.appendChild(aTable);
+
+    var topicsNode = document.getElementById("topics");
+    var script_sidebarNode = document.getElementById("script_sidebar");
+    if (script_sidebarNode && topicsNode)
+      script_sidebarNode.insertBefore(nodeDiv, topicsNode);
+  }
+
+  /**
+   *
+   */
+  var abort;
 
   var full_descriptionNode = document.getElementById("full_description");
   if (full_descriptionNode) {
 
-    var tableNodes = full_descriptionNode.querySelectorAll("table");
-    for (var i = 0, tableNode; tableNode = tableNodes[i]; i++) {
+    var thNodes = full_descriptionNode.querySelectorAll("table > tbody > tr > th:first-child");
+    for (var i = 0, thNode; thNode = thNodes[i]; i++) {
+      if (thNode.textContent == "Sticky Topics") {
+        var tableNode = thNode.parentNode.parentNode.parentNode;
+        abort = false;
 
-      var thNodes = tableNode.querySelectorAll("th");
-      if (thNodes.length == 2 && thNodes[0].textContent == "Sticky Topics" && thNodes[1].textContent == "Posts") {
+        var trNodes = tableNode.querySelectorAll("tbody tr");
+        for (var j = 0, trNode; trNode = trNodes[j]; j++) {
 
-        var trNodes = tableNode.querySelectorAll("tr");
-        if (trNodes.length > 0) {
-          for (var j = 0, trNode; trNode = trNodes[j]; j++) {
-            var tid = null;
+          var tid = null;
 
-            var tdNodes = trNode.querySelectorAll("td");
-            if (tdNodes.length == 2) {
-              for (var k = 0, tdNode; tdNode = tdNodes[k]; k++) {
-                switch (k) {
-                  case 0:
-                    var aNodes = tdNode.querySelectorAll("a");
-                    if (aNodes.length == 1) {
-                      for (var l = 0, aNode; aNode = aNodes[l]; l++) {
-                        var matches = aNode.href.match(/^(?:https?:\/\/userscripts\.org)\/topics\/(\d+)/i);
-                        if (matches) {
-                          tid = matches[1];
-                          aNode.href = "/topics/" + tid + aNode.hash;
+          var tdNodes = trNode.querySelectorAll("td");
+          if (tdNodes.length == 2) {
+            for (var k = 0, tdNode; tdNode = tdNodes[k]; k++) {
+              switch (k) {
+                case 0:
+                  var aNodes = tdNode.querySelectorAll("a");
+                  if (aNodes.length == 1) {
+                    var aNode = aNodes[0];
 
-                          var textContent = aNode.textContent;
-                          if (textContent.length > 30)
-                            aNode.textContent = textContent.substr(0, 27) + "...";
-                        }
-                        else
-                          abort = true;
-                      }
+                    var matches = aNode.href.match(/^(?:https?:\/\/userscripts\.org)\/topics\/(\d+)/i);
+                    if (matches) {
+                      tid = matches[1];
+                      aNode.href = "/topics/" + tid + aNode.hash;
+
+                      var textContent = aNode.textContent;
+                      if (textContent.length > 30)
+                        aNode.textContent = textContent.substr(0, 27) + "...";
                     }
-                    else
-                      abort = true;
-                    break;
+                  }
+                  else {
+                    console.info('ABORT: Too many links');
+                    abort = true;
+                  }
+                  break;
 
-                  case 1:
-                    var aNodes = tdNode.querySelectorAll("a");
-                    if (aNodes.length > 0) {
-                      if (aNodes.length == 1) {
-                        aNode = aNodes[0];
+                case 1:
+                  var aNodes = tdNode.querySelectorAll("a");
+                  if (aNodes.length > 0) {
+                    if (aNodes.length == 1) {
+                      aNode = aNodes[0];
 
-                        aNode.href = "/topics/" + tid + "#posts-last";
-                        aNode.textContent = "\u00BB";
-                      }
-                      else
-                        abort = true;
+                      aNode.href = "/topics/" + tid + "#posts-last";
+                      aNode.textContent = "\u00BB";
                     }
                     else {
-                      tdNode.textContent = "\u2013";
+                      console.info('ABORT: Too many links');
+                      abort = true;
                     }
-
-                    break;
-                  default:
-                    abort = true;
-                }
-
+                  }
+                  else
+                    tdNode.textContent = "\u2013";
+                  break;
               }
             }
-
-            if (!abort) {
-              /**
-              * Add styling
-              */
-              var css = [
-                  '#right #stickytopics { font-size: 0.9em; }',
-                  '#right #stickytopics td:last-child { text-align: right; }'
-
-              ].join('');
-
-              var nodeStyle = document.createElement("style");
-              nodeStyle.type = "text/css";
-              nodeStyle.textContent = css;
-
-              document.head.appendChild(nodeStyle);
-
-              /**
-              * Symmetry with USO
-              */
-              tableNode.width = "100%";
-
-
-              /**
-              * Move the table
-              */
-              var nodeDiv = document.createElement("div");
-              nodeDiv.id = "stickytopics";
-
-              nodeDiv.appendChild(tableNode);
-
-              var topicsNode = document.getElementById("topics");
-              var script_sidebarNode = document.getElementById("script_sidebar");
-              if (script_sidebarNode && topicsNode) {
-                script_sidebarNode.insertBefore(nodeDiv, topicsNode);
-              }
-
-              abort = false;
-            }
-
           }
         }
+
+        if (!abort)
+          moveTable(tableNode);
       }
     }
+
   }
 })();
