a1368f4e222a825ea9f34a90aa716d76bb7c8e20
Martii <thalamew@hotmail.com>
2014-02-14 14:42:45 -0700

    385863.918585 uso - More Pages [0.0.0](/scripts/diff/385863/918585)
    - Initial metadata block with IIFE
    
    385863.918585
    
    SHA1:ea368688f9620f167dfb4eb32b3e87c65ac054f7

---
+++ src/uso/More Pages/uso - More Pages.user.js
@@ -0,0 +1,23 @@
+(function() {
+  "use strict"
+
+// ==UserScript==
+// @name          uso - More Pages
+// @namespace     http://userscripts.org/users/37004
+// @description   Adds a link to the next page and optionally if detected as missing
+// @copyright     2010+, Marti Martz (http://userscripts.org/users/37004)
+// @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
+// @license       (CC); http://creativecommons.org/licenses/by-nc-sa/3.0/
+// @icon          http://www.gravatar.com/avatar.php?gravatar_id=e615596ec6d7191ab628a1f0cec0006d&r=PG&s=48&default=identicon
+// @version       0.0.0
+// @icon          https://www.gravatar.com/avatar/e615596ec6d7191ab628a1f0cec0006d?r=PG&s=48&default=identicon
+
+// @include  http://userscripts.org/*
+// @include  https://userscripts.org/*
+
+// @grant  none
+
+// ==/UserScript==
+
+
+})();
