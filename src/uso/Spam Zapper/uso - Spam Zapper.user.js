19bd5e1d4bd17277438acd813f8aa5186e805cad
Martii <thalamew@hotmail.com>
2014-02-25 14:20:39 -0700

    398715.943468 uso - Spam Zapper [1.0.8](/scripts/diff/398715/943468)
    - Give a visual indication when hamming in polls. This doesn't mean the poll is invalidated however your vote will show temporarily on the screen as being hammed with a background request... on a manual page refresh it may reshow as being potentially spam if other votes are present.
    
    398715.943468
    
    SHA1:5519a56617566431aeea0da2c60bdedf74e38e23

--- src/uso/Spam Zapper/uso - Spam Zapper.user.js
+++ src/uso/Spam Zapper/uso - Spam Zapper.user.js
@@ -10,7 +10,7 @@
 // @contributor     Ryan Chatham (http://userscripts.org/users/220970)
 // @license         GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @license         Creative Commons; http://creativecommons.org/licenses/by-nc-sa/3.0/
-// @version         1.0.7
+// @version         1.0.8
 // @icon            https://www.gravatar.com/avatar/e615596ec6d7191ab628a1f0cec0006d?r=PG&s=48&default=identicon
 
 // @include  http://userscripts.org/posts*
@@ -256,6 +256,12 @@
               }
             }
             aNode.classList.remove("spam");
+            aNode.classList.remove("bad-ham");
+
+            var spampollNode = aNode.querySelector(".spam_poll");
+            if (spampollNode)
+              spampollNode.classList.add("hide");
+
             delete postids[target_id];
             GM_setValue("postids", JSON.stringify(postids, null, ""));
 
