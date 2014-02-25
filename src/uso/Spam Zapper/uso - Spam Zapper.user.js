9a2625d9fdabf27b4cf78cf8dbcdf0575db25907
Martii <thalamew@hotmail.com>
2014-02-24 21:46:07 -0700

    398715.942360 uso - Spam Zapper [0.0.0](/scripts/diff/398715/942360)
    - Initial metadata block commit with IIFE
    
    398715.942360
    
    SHA1:c8b4b7654e530dc5896d32a47f165732e941f9aa

---
+++ src/uso/Spam Zapper/uso - Spam Zapper.user.js
@@ -0,0 +1,38 @@
+(function () {
+  "use strict";
+
+// ==UserScript==
+// @name            uso - Spam Zapper
+// @namespace       http://userscripts.org/users/37004
+// @description     Background requests for spam buttons and native USO alteration to the post when marked as spam.
+// @copyright       2013+, Marti Martz (http://userscripts.org/users/37004)
+// @contributor     Jesse Andrews (http://userscripts.org/users/2)
+// @contributor     Ryan Chatham (http://userscripts.org/users/220970)
+// @license         GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
+// @license         Creative Commons; http://creativecommons.org/licenses/by-nc-sa/3.0/
+// @version         0.0.0
+// @icon            http://www.gravatar.com/avatar/e615596ec6d7191ab628a1f0cec0006d?r=PG&s=48&default=identicon
+
+// @include  http://userscripts.org/posts*
+// @include  http://userscripts.org/topics/*
+// @include  http://userscripts.org/forums/*
+// @include  http://userscripts.org/users/*/posts*
+
+// @include  https://userscripts.org/posts*
+// @include  https://userscripts.org/topics/*
+// @include  https://userscripts.org/forums/*
+// @include  https://userscripts.org/users/*/posts*
+
+// @updateURL   file:
+// @installURL  file:
+// @downloadURL file:
+
+// @grant  GM_addStyle
+// @grant  GM_deleteValue
+// @grant  GM_getValue
+// @grant  GM_setValue
+
+// ==/UserScript==
+
+
+})();
