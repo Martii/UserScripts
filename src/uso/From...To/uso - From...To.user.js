1c0fd57453a500497f9d61ab95ca7c91f0cd011e
Martii <thalamew@hotmail.com>
2014-03-15 04:51:38 -0600

    396490.970559 uso - From…To [1.0.6](/scripts/diff/396490/970559)
    - Script integrity check for scripts that don't maintain USO DOM integrity (e.g. [this script](/scripts/show/97145) with [this current version](/scripts/version/97145/917522.user.js) reported from [this post](/posts/609546)).
    Since that script is generating a non-consistent DOM... applying a compatibility/integrity fix depending on script execution order.
    
    That script and any other that utilize the subtitle should do some follow-up on their end:
    Strip off the trailing space from the .subtitle last child before appending any elementsAdd the elements needed for that script to workRe-append a trailing space to maintain DOM integrity and sharing of the subtitleAnd most importantly... Fix CSS rules from being too general... e.g. his *:hover is currently bleeding over into the DOM elements created from this script.
    
    396490.970559
    
    SHA1:34cf0a439ac0453139dea25693c541344c5277c3

--- src/uso/From...To/uso - From...To.user.js
+++ src/uso/From...To/uso - From...To.user.js
@@ -8,7 +8,7 @@
 // @copyright     2014+, Marti Martz (http://userscripts.org/users/37004)
 // @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @license       Creative Commons; http://creativecommons.org/licenses/by-nc-nd/3.0/
-// @version       1.0.5
+// @version       1.0.6
 // @icon          https://www.gravatar.com/avatar/e615596ec6d7191ab628a1f0cec0006d?r=PG&s=48&default=identicon
 
 // @include       http://userscripts.org/*
@@ -40,7 +40,8 @@
       null
     );
     if (abbrNodes.snapshotLength > 0) {
-      thisNode.lastChild.textContent = thisNode.lastChild.textContent.replace(/\s*$/, "");
+      if (thisNode.lastChild.nodeType == 3)
+        thisNode.lastChild.textContent = thisNode.lastChild.textContent.replace(/\s*$/, "");
 
       var firstNode = abbrNodes.snapshotItem(0);
       var lastNode = abbrNodes.snapshotItem(abbrNodes.snapshotLength - 1);
