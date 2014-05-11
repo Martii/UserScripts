8125ad267bc994a52cec6f92a4cc04fa05dc178b
Martii <thalamew@hotmail.com>
2014-05-11 03:57:28 -0600

    162319.1077420 uso - Retry [0.1.2](/scripts/diff/162319/1077420)
    - Modified re include
    - White space cleanup
    
    162319.1077420
    
    SHA1:43f2a335f32b4cb33556c263ddf9784750dd7f03

--- src/uso/Retry/uso - Retry.user.js
+++ src/uso/Retry/uso - Retry.user.js
@@ -9,11 +9,13 @@
 // @copyright   2011+, Marti Martz (http://userscripts.org/users/37004)
 // @license     GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @license     Creative Commons; http://creativecommons.org/licenses/by-nc-nd/3.0/
-// @version     0.1.1
+// @version     0.1.2
 // @icon https://www.gravatar.com/avatar/e615596ec6d7191ab628a1f0cec0006d?r=PG&s=48&default=identicon
 
-// @include /^https?://userscripts\.org/?/
+// @include /^https?://userscripts\.org(?::\d{1,5})?/?/
+
 // @include http://userscripts.org/*
+
 // @include https://userscripts.org/*
 
 // ==/UserScript==
