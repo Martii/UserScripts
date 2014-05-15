5871dd9a8869f1a6a8f96a96c7063d5987344dcd
Martii <thalamew@hotmail.com>
2014-05-15 16:36:02 -0600

    Attempt to trigger a syncop with `@version` bump
    Typo fixered

--- src/RFC 2606§3/Hello, World/RFC 2606§3 - Hello, World.user.js
+++ src/RFC 2606§3/Hello, World/RFC 2606§3 - Hello, World.user.js
@@ -7,7 +7,7 @@
 // @copyright     2007+, Marti Martz (http://userscripts.org/users/37004)
 // @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @license       (CC); http://creativecommons.org/licenses/by-nc-sa/3.0/
-// @version       0.0.0
+// @version       0.0.1
 // @icon          https://s3.amazonaws.com/uso_ss/icon/13701/large.png
 
 // @include   http://www.example.com/*
@@ -20,4 +20,4 @@
 
 })();
 
-// Attemp to trigger a syncop for OUJS
+// Attempt to trigger a syncop for OUJS
