4347f16b0965181523ea2d7b3dc85be1b749ffec
Martii <thalamew@hotmail.com>
2014-05-19 19:30:36 -0600

    uso - Sticky Topics 1.0.10
    * Version bump
    * Add alternate port for GF discoverability
    * Remove GMU override for now
    * Assets for this script

--- src/uso/Sticky Topics/uso - Sticky Topics.user.js
+++ src/uso/Sticky Topics/uso - Sticky Topics.user.js
@@ -8,17 +8,16 @@
 // @copyright     2014+, Marti Martz (http://userscripts.org/users/37004)
 // @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @license       Creative Commons; http://creativecommons.org/licenses/by-nc-nd/3.0/
-// @version       1.0.9.1esr1
+// @version       1.0.10
 // @icon          https://www.gravatar.com/avatar/e615596ec6d7191ab628a1f0cec0006d?r=PG&s=48&default=identicon
 
 // @include       /^https?://userscripts\.org(?::\d{1,5})?/scripts/show/?/
 
-// @include       http://userscripts.org/scripts/show/*
-// @include       https://userscripts.org/scripts/show/*
+// @include       http://userscripts.org:8080/scripts/show/*
 
-// @updateURL   http://userscripts.org:8080/scripts/source/378796.meta.js
-// @installURL  http://userscripts.org:8080/scripts/source/378796.user.js
-// @downloadURL http://userscripts.org:8080/scripts/source/378796.user.js
+// @include       http://userscripts.org/scripts/show/*
+
+// @include       https://userscripts.org/scripts/show/*
 
 // @grant  none
 
