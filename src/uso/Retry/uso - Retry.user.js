5c4a819cada394715fe4ee91999ed71d5e28c704
Martii <thalamew@hotmail.com>
2014-06-29 14:19:01 -0600

    uso - Retry 0.1.4
    * Set GPL as primary for OUJS and GM Standard

--- src/uso/Retry/uso - Retry.user.js
+++ src/uso/Retry/uso - Retry.user.js
@@ -7,9 +7,9 @@
 // @namespace   http://userscripts.org/users/37004
 // @description Auto-Refreshes current URI on USO when a non-200 response code is encountered.
 // @copyright   2011+, Marti Martz (http://userscripts.org/users/37004)
-// @license     GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @license     Creative Commons; http://creativecommons.org/licenses/by-nc-nd/3.0/
-// @version     0.1.3
+// @license     GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
+// @version     0.1.4
 // @icon  https://www.gravatar.com/avatar/e615596ec6d7191ab628a1f0cec0006d?r=PG&s=48&default=identicon
 
 // @include /^https?://userscripts\.org(?::\d{1,5})?/?/
