d08d30b4f3510322eb10db7e96856af3ed4e8d37
Martii <thalamew@hotmail.com>
2014-07-08 01:17:30 -0600

    RFC 2606§3 - @license and @licence (recovered) Unit Test
    * Initialize this on GH for testing purposes

---
+++ src/RFC 2606§3/@license and @licence (recovered) Unit Test/RFC 2606§3 - @license and @licence (recovered) Unit Test.user.js
@@ -0,0 +1,47 @@
+(function () {
+
+// ==UserScript==
+// @name          RFC 2606§3 - @license and @licence (recovered) Unit Test
+// @namespace     http://localhost.localdomain
+// @description   Tests out non-unique keys 2
+// @description   Tests out non-unique keys
+// @copyright     2007+, Marti Martz (http://userscripts.org/users/37004)
+// @licence       (CC); http://creativecommons.org/licenses/by-nc-sa/3.0/
+// @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
+// @version       2014.07.08.0001
+// @icon          https://s3.amazonaws.com/uso_ss/icon/13701/large.png
+
+// @homepage  http://localhost:8080/scripts/Marti/RFC_2606%C2%A73_-_license_and_licence_%28recovered%29_Unit_Test
+// @homepageURL  http://localhost:8080/scripts/Marti/RFC_2606%C2%A73_-_license_and_licence_%28recovered%29_Unit_Test
+// @homepageURL  http://localhost:8080/scripts/Marti/RFC_2606%C2%A73_-_license_and_licence_%28recovered%29_Unit_Test
+// @homepageURL  http://localhost:8080/scripts/Marti/RFC_2606%C2%A73_-_license_and_licence_%28recovered%29_Unit_Test
+// @homepageURL  https://openuserjs.org/scripts/Marti/RFC_2606%C2%A73_-_license_and_licence_%28recovered%29_Unit_Test
+
+// @supportURL   http://localhost:8080/scripts/Marti/RFC_2606%C2%A73_-_license_and_licence_(recovered)_Unit_Test/issues
+// @supportURL   mailto:user@example.com
+
+// @include   http://www.example.com/*
+// @include   http://www.example.net/*
+// @include   http://www.example.org/*
+
+// @oujs:author Marti
+// @oujs:collaborator  Martii
+// @oujs:collaborator  Martii
+// @oujs:collaborator  jerone
+// @oujs:collaborator  sizzle
+// @oujs:collaborator  Zren
+
+// @gf:author ThisShouldNotTriggerCollaboration
+// @gf:collaborator ThisShouldNotShowEventually
+
+// @uso:script 0
+
+// @unwrap
+
+// @require http://localhost:8080/libs/src/Marti/GM_setStyle.js
+
+// ==/UserScript==
+
+  // Do Nothing
+
+})();
