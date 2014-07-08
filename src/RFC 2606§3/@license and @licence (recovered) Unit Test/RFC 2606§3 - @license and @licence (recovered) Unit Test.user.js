b03e1c0a188115963ec34f220694c5c355177bed
Martii <thalamew@hotmail.com>
2014-07-08 13:13:29 -0600

    RFC 2606§3 - @license and @licence (recovered) Unit Test
    * bunch of duplicate keys added

--- src/RFC 2606§3/@license and @licence (recovered) Unit Test/RFC 2606§3 - @license and @licence (recovered) Unit Test.user.js
+++ src/RFC 2606§3/@license and @licence (recovered) Unit Test/RFC 2606§3 - @license and @licence (recovered) Unit Test.user.js
@@ -8,14 +8,19 @@
 // @copyright     2007+, Marti Martz (http://userscripts.org/users/37004)
 // @licence       (CC); http://creativecommons.org/licenses/by-nc-sa/3.0/
 // @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
-// @version       2014.07.08.0001
+// @version       2014.07.08.0002
 // @icon          https://s3.amazonaws.com/uso_ss/icon/13701/large.png
+// @defaulticon   https://s3.amazonaws.com/uso_ss/icon/13701/large.png
+// @iconURL       https://s3.amazonaws.com/uso_ss/icon/13701/large.png?test
 
-// @homepage  http://localhost:8080/scripts/Marti/RFC_2606%C2%A73_-_license_and_licence_%28recovered%29_Unit_Test
+// @source       http://localhost:8080/scripts/Marti/RFC_2606%C2%A73_-_license_and_licence_%28recovered%29_Unit_Test
+// @website      http://localhost:8080/scripts/Marti/RFC_2606%C2%A73_-_license_and_licence_%28recovered%29_Unit_Test
+// @homepage     http://localhost:8080/scripts/Marti/RFC_2606%C2%A73_-_license_and_licence_%28recovered%29_Unit_Test
 // @homepageURL  http://localhost:8080/scripts/Marti/RFC_2606%C2%A73_-_license_and_licence_%28recovered%29_Unit_Test
 // @homepageURL  http://localhost:8080/scripts/Marti/RFC_2606%C2%A73_-_license_and_licence_%28recovered%29_Unit_Test
 // @homepageURL  http://localhost:8080/scripts/Marti/RFC_2606%C2%A73_-_license_and_licence_%28recovered%29_Unit_Test
 // @homepageURL  https://openuserjs.org/scripts/Marti/RFC_2606%C2%A73_-_license_and_licence_%28recovered%29_Unit_Test
+// @homepageURL  https://github.com/Martii/UserScripts/tree/master/src/RFC%202606%C2%A73/%40license%20and%20%40licence%20%28recovered%29%20Unit%20Test
 
 // @supportURL   http://localhost:8080/scripts/Marti/RFC_2606%C2%A73_-_license_and_licence_(recovered)_Unit_Test/issues
 // @supportURL   mailto:user@example.com
