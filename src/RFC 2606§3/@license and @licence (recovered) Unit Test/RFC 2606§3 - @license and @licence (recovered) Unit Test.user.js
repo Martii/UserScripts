ac5a65dc95ebb4d7ec5b6485ca8e867e5b0e6626
Martii <thalamew@q.com>
2014-07-27 18:57:32 -0600

    Twiddling

--- src/RFC 2606§3/@license and @licence (recovered) Unit Test/RFC 2606§3 - @license and @licence (recovered) Unit Test.user.js
+++ src/RFC 2606§3/@license and @licence (recovered) Unit Test/RFC 2606§3 - @license and @licence (recovered) Unit Test.user.js
@@ -14,7 +14,7 @@
 // @copyright     2007+, Marti Martz (http://userscripts.org/users/37004)
 // @licence       (CC); http://creativecommons.org/licenses/by-nc-sa/3.0/
 // @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
-// @version       2014.07.27.0001
+// @version       2014.07.27.0003
 // @icon          https://s3.amazonaws.com/uso_ss/icon/13701/large.png
 // @defaulticon   https://s3.amazonaws.com/uso_ss/icon/13701/large.png
 // @iconURL       https://s3.amazonaws.com/uso_ss/icon/13701/large.png?test
@@ -35,6 +35,7 @@
 // @include   http://www.example.net/*
 // @include   http://www.example.org/*
 
+// @author Marti Martz (https://github.com/martii)
 // @oujs:author Marti
 // @oujs:collaborator  jerone
 // @oujs:collaborator  sizzle
