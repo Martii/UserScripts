070cfdc91e64eefbc1f3f2457e47c97ea4166567
Martii <thalamew@q.com>
2014-07-31 03:51:44 -0600

    oujs - Meta View (Alpha 2) 2.2.3a2.1
    * Flip upmixing to ignore locale

--- src/oujs/Meta View/oujs - Meta View (Alpha2).user.js
+++ src/oujs/Meta View/oujs - Meta View (Alpha2).user.js
@@ -8,7 +8,7 @@
 // @copyright     2014+, Marti Martz (http://userscripts.org/users/37004)
 // @license       (CC); http://creativecommons.org/licenses/by-nc-sa/3.0/
 // @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
-// @version       2.2.3a2
+// @version       2.2.3a2.1
 // @icon          https://www.gravatar.com/avatar/7ff58eb098c23feafa72e0b4cd13f396?r=G&s=48&default=identicon
 
 // @homepageURL  https://github.com/Martii/UserScripts/tree/master/src/oujs/Meta%20View
@@ -74,11 +74,15 @@
           name = matches[1];
           value = matches[2];
 
+          name = name.split(/:/);
+          key = name[0];
+          locale = name[1] || '';
+
           if (aNormalize) {
             // Upmix from...
             switch (block) {
               case 'UserScript':
-                switch (name) {
+                switch (key) {
                   case 'homepage':
                   case 'source':
                   case 'website':
@@ -93,10 +97,6 @@
                 }
             }
           }
-
-          name = name.split(/:/);
-          key = name[0];
-          locale = name[1] || '';
 
           if (key) {
             if (!head[block]) {
@@ -118,7 +118,6 @@
               }
               head[key].push(value || '');
             }
-
           }
         }
       }
