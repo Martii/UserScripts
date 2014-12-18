2768176423056831433f47dec2f52dbb9c26a548
Martii <thalamew@q.com>
2014-12-18 03:37:14 -0700

    oujs - JsBeautify 0.1.2b
    * Add non-secure for injection
    * Add `Wrap` button to toolbar... dual-state e.g. reversible *(on or off... this is an Ace feature but still part of beautification)*
    * Change `Beautify` button to be multi-state... if no edits detected then reversible otherwise additive only.
    * Change gutter and `Submit Code...` buttons to altered color when source is edited

--- src/oujs/JsBeautify/oujs - JsBeautify.user.js
+++ src/oujs/JsBeautify/oujs - JsBeautify.user.js
@@ -9,11 +9,14 @@
 // @contributor   Chris Holland (https://github.com/Zren)
 // @license       (CC); http://creativecommons.org/licenses/by-nc-sa/3.0/
 // @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
-// @version       0.1.1b
+// @version       0.1.2b
 // @icon          https://gravatar.com/avatar/7ff58eb098c23feafa72e0b4cd13f396?s=48
 
 // @include  https://openuserjs.org/scripts/*/*/source
 // @include  https://openuserjs.org/libs/*/*/source
+
+// @include  http://openuserjs.org/scripts/*/*/source
+// @include  http://openuserjs.org/libs/*/*/source
 
 // @include  http://localhost:8080/scripts/*/*/source
 // @include  http://localhost:8080/libs/*/*/source
@@ -35,6 +38,32 @@
     }) + '\n';
   }
 
+  var
+    rawSource = ace.edit('editor').getSession().getValue(),
+    isBlocking,
+    hasChanged = false
+  ;
+
+  ace.edit('editor').getSession().on('change', function (aE) {
+    if (isBlocking)
+      return;
+
+    if (!hasChanged) {
+      hasChanged = true;
+
+      var submit_codeNode = document.querySelector('button#submit_code');
+      if (submit_codeNode) {
+        submit_codeNode.classList.remove('btn-success');
+        submit_codeNode.classList.add('btn-warning');
+      }
+
+      var ace_gutterLayer = document.querySelector('.ace_gutter-layer');
+      if (ace_gutterLayer) {
+        ace_gutterLayer.classList.add('btn-warning');
+      }
+    }
+  });
+
   var hookNode = document.querySelector('div.container-fluid div.row div.col-md-12');
   if (hookNode) {
     var beautifyNodeInput = document.createElement('input');
@@ -46,22 +75,76 @@
     beautifyNodeInput.addEventListener('click', function (aE) {
       var thisNode = document.querySelector('pre#editor');
       if (thisNode) {
-        ace.edit('editor').getSession().setValue(beautify(ace.edit('editor').getSession().getValue()));
 
         var submit_codeNode = document.querySelector('button#submit_code');
-        if (submit_codeNode) {
-          submit_codeNode.classList.remove('btn-success');
-          submit_codeNode.classList.add('btn-warning');
+        var ace_gutterLayer = document.querySelector('.ace_gutter-layer');
+
+        if (aE.target.classList.contains('active') && !hasChanged) {
+          isBlocking = true;
+          ace.edit('editor').getSession().setValue(rawSource);
+          aE.target.classList.remove('active');
+          isBlocking = false;
+
+          if (submit_codeNode) {
+            submit_codeNode.classList.add('btn-success');
+            submit_codeNode.classList.remove('btn-warning');
+          }
+
+          if (ace_gutterLayer) {
+            ace_gutterLayer.classList.remove('btn-warning');
+          }
+
+        }
+        else {
+          isBlocking = true;
+          aE.target.classList.add('active');
+          ace.edit('editor').getSession().setValue(beautify(hasChanged ? ace.edit('editor').getSession().getValue() : rawSource));
+          isBlocking = false;
+
+          if (submit_codeNode) {
+            submit_codeNode.classList.remove('btn-success');
+            submit_codeNode.classList.add('btn-warning');
+          }
+
+          if (ace_gutterLayer) {
+            ace_gutterLayer.classList.add('btn-warning');
+          }
         }
 
-        var ace_gutterLayer = document.querySelector('.ace_gutter-layer');
-        if (ace_gutterLayer) {
-          ace_gutterLayer.classList.add('btn-warning');
-        }
+        aE.target.blur();
       }
     });
 
-    hookNode.appendChild(beautifyNodeInput);
+
+    var wrappedNodeInput =  document.createElement('input');
+    wrappedNodeInput.classList.add('btn');
+    wrappedNodeInput.classList.add('btn-success');
+    wrappedNodeInput.id = 'wrap';
+    wrappedNodeInput.value = 'Wrap';
+    wrappedNodeInput.type = 'button';
+    wrappedNodeInput.addEventListener('click', function (aE) {
+      var thisNode = document.querySelector('pre#editor');
+      if (thisNode) {
+        if (ace.edit('editor').getSession().getUseWrapMode()) {
+          ace.edit('editor').getSession().setUseWrapMode(false);
+          aE.target.classList.remove('active');
+        }
+        else {
+          ace.edit('editor').getSession().setUseWrapMode(true);
+          aE.target.classList.add('active');
+        }
+
+        aE.target.blur();
+      }
+    });
+
+    var toolbarNodeDiv = document.createElement('div');
+    toolbarNodeDiv.classList.add('btn-toolbar');
+
+    toolbarNodeDiv.appendChild(wrappedNodeInput);
+    toolbarNodeDiv.appendChild(beautifyNodeInput);
+
+    hookNode.appendChild(toolbarNodeDiv);
   }
 
 })();
