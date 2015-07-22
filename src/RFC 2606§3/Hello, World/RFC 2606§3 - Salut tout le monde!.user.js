e2450c76216739057edc599c4457f9961f7a4eb5
Martii <thalamew@q.com>
2015-07-22 07:11:44 -0600

    RFC 2606§3 - Salut tout le monde! 0.0.0
    a.k.a. Hello, World! :)
    
    * French only `@name` Unit Test

---
+++ src/RFC 2606§3/Hello, World/RFC 2606§3 - Salut tout le monde!.user.js
@@ -0,0 +1,26 @@
+(function () {
+
+// ==UserScript==
+// @name:fr       RFC 2606§3 - Salut tout le monde!
+// @namespace     http://localhost.localdomain
+// @description   JavaScript alert box saying Hello, World!
+// @copyright     2007+, Marti Martz (http://userscripts.org/users/37004)
+// @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
+// @license       (CC); http://creativecommons.org/licenses/by-nc-sa/3.0/
+// @version       0.0.0
+// @icon          https://s3.amazonaws.com/uso_ss/icon/13701/large.png
+
+// @homepageURL   https://github.com/Martii/UserScripts/tree/master/src/RFC%202606%C2%A73/Hello%2C%20World
+// @homepageURL   https://openuserjs.org/scripts/Marti/RFC_2606%C2%A73_-_Hello,_World!
+// @homepageURL   http://userscripts.org/scripts/show/13701
+// @supportURL    http://userscripts.org/topics/23566
+
+// @include   http://www.example.com/*
+// @include   http://www.example.net/*
+// @include   http://www.example.org/*
+
+// ==/UserScript==
+
+  alert('Salut tout le monde!');
+
+})();
