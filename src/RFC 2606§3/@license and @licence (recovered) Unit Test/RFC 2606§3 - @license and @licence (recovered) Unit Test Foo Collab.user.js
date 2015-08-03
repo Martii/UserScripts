63d49fa2e42cd91e440da38ef2e434de026eb2c0
Martii <thalamew@q.com>
2015-08-03 14:45:22 -0600

    RFC 2606§3 - @license and @licence (recovered) Unit Test Foo Collab
    * Add a Unit Test to test out current foo'd collaboration

---
+++ src/RFC 2606§3/@license and @licence (recovered) Unit Test/RFC 2606§3 - @license and @licence (recovered) Unit Test Foo Collab.user.js
@@ -0,0 +1,82 @@
+(function () {
+
+// ==UserScript==
+// @run-at             document-end
+
+// @namespace          http://localhost.localdomain
+
+// @name:fr            RFC 2606§3 - @license et @licence (récupéré) Test unitaire Foo Collab 3
+// @name               RFC 2606§3 - @license and @licence (recovered) Unit Test Foo Collab 2
+// @name               RFC 2606§3 - @license and @licence (recovered) Unit Test Foo Collab
+// @name:de-DE         RFC 2606§3 - @license und @licence (wiederhergestellt) Komponententest  Foo Collab
+// @name:fr            RFC 2606§3 - @license et @licence (récupéré) Test unitaire Foo Collab 2
+// @name:fr            RFC 2606§3 - @license et @licence (récupéré) Test unitaire Foo Collab
+
+// @description        Tests out non-unique keys 2 and collaboration
+// @description        Tests out non-unique keys and collaboration
+// @description:de-DE  Tests, nicht eindeutige Schlüssel
+// @description:fr     Tests de clefs non unique
+
+// @copyright     2007+, Marti Martz (http://userscripts.org/users/37004)
+// @licence       (CC); http://creativecommons.org/licenses/by-nc-sa/3.0/
+// @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
+// @version       2015.07.19.002
+// @icon          https://s3.amazonaws.com/uso_ss/icon/13701/large.png
+// @defaulticon   https://s3.amazonaws.com/uso_ss/icon/13701/large.png
+// @iconURL       https://s3.amazonaws.com/uso_ss/icon/13701/large.png?test
+
+// @source       http://localhost:8080/scripts/Marti/RFC_2606%C2%A73_-_license_and_licence_%28recovered%29_Unit_Test
+// @website      http://localhost:8080/scripts/Marti/RFC_2606%C2%A73_-_license_and_licence_%28recovered%29_Unit_Test
+// @homepage     http://localhost:8080/scripts/Marti/RFC_2606%C2%A73_-_license_and_licence_%28recovered%29_Unit_Test
+// @homepageURL  http://localhost:8080/scripts/Marti/RFC_2606%C2%A73_-_license_and_licence_%28recovered%29_Unit_Test
+// @homepageURL  http://localhost:8080/scripts/Marti/RFC_2606%C2%A73_-_license_and_licence_%28recovered%29_Unit_Test
+// @homepageURL  http://localhost:8080/scripts/Marti/RFC_2606%C2%A73_-_license_and_licence_%28recovered%29_Unit_Test
+// @homepageURL  https://openuserjs.org/scripts/Marti/RFC_2606%C2%A73_-_license_and_licence_%28recovered%29_Unit_Test
+// @homepageURL  https://github.com/Martii/UserScripts/tree/master/src/RFC%202606%C2%A73/%40license%20and%20%40licence%20%28recovered%29%20Unit%20Test
+
+// @supportURL   http://localhost:8080/scripts/Marti/RFC_2606%C2%A73_-_license_and_licence_(recovered)_Unit_Test/issues
+// @supportURL   mailto:user@example.com
+
+// @include   http://www.example.com/*
+// @include   http://www.example.net/*
+// @include   http://www.example.org/*
+
+//@author Marti Martz (https://github.com/martii)
+//@oujs:author Marti
+//@oujs:collaborator  jerone
+//@oujs:collaborator  sizzle
+//@oujs:collaborator  sizzle
+//@oujs:collaborator  Zren
+
+// @oujs:author: Marti
+// @oujs:author: Marti
+// @oujs:collaborator:  jerone
+// @oujs:collaborator:  sizzle
+// @oujs:collaborator:  sizzle
+// @oujs:collaborator:  Zren
+
+
+// @gf:author: ThisShouldNotTriggerCollaboration
+// @gf:collaborator: ThisShouldNotShowEventually
+
+// @uso:script: 0
+
+// @unwrap
+
+// @someotherkey:fr  Tests de clefs non unique 2
+
+//@require http://localhost:8080/libs/src/Marti/GM_setStyle.js
+
+// ==/UserScript==
+
+// ==OpenUserJS==
+// @author Martii
+// @author Marti
+// @collaborator sizzle
+// @collaborator jerone
+// ==/OpenUserJS==
+
+
+  // Do Nothing
+
+})();
