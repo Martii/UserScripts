4f33ae21591f18d0b357b4f3c79be0ff011d3641
Martii <thalamew@q.com>
2015-10-30 13:27:59 -0600

    RFC 2606§3 - GM_listValues Unit Test 0.0.0
    * Initial commit

---
+++ src/RFC 2606§3/GM_listValues Unit Test/RFC 2606§3 - GM_listValues Unit Test.user.js
@@ -0,0 +1,29 @@
+(function () {
+
+// ==UserScript==
+// @name          RFC 2606§3 - GM_listValues Unit Test
+// @namespace     http://localhost.localdomain
+// @description   Tests GM_listValues by displaying an alert of the values
+// @copyright     2007+, Marti Martz (http://userscripts.org/users/37004)
+// @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
+// @license       (CC); http://creativecommons.org/licenses/by-nc-sa/3.0/
+// @version       0.0.0
+// @icon          https://www.gravatar.com/avatar/7ff58eb098c23feafa72e0b4cd13f396?r=G&s=48&default=identicon
+
+// @include   http://www.example.com/*
+// @include   http://www.example.net/*
+// @include   http://www.example.org/*
+
+// @grant GM_setValue
+// @grant GM_listValues
+
+// ==/UserScript==
+
+  GM_setValue('one', 1);
+  GM_setValue('two', 2);
+  GM_setValue('three', 3);
+
+  alert(GM_listValues());
+
+
+})();
