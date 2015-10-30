6391d61f0906ab3f8fe42c7eecc9a92c4d770c1d
Martii <thalamew@q.com>
2015-10-30 13:30:34 -0600

    RFC 2606§3 - GM_listValues Unit Test 0.0.1
    * Flip home location

--- src/RFC 2606§3/GM_listValues Unit Test/RFC 2606§3 - GM_listValues Unit Test.user.js
+++ src/RFC 2606§3/GM_listValues Unit Test/RFC 2606§3 - GM_listValues Unit Test.user.js
@@ -4,10 +4,10 @@
 // @name          RFC 2606§3 - GM_listValues Unit Test
 // @namespace     http://localhost.localdomain
 // @description   Tests GM_listValues by displaying an alert of the values
-// @copyright     2007+, Marti Martz (http://userscripts.org/users/37004)
+// @copyright     2007+, Marti Martz (https://openuserjs.org/users/Marti)
 // @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @license       (CC); http://creativecommons.org/licenses/by-nc-sa/3.0/
-// @version       0.0.0
+// @version       0.0.1
 // @icon          https://www.gravatar.com/avatar/7ff58eb098c23feafa72e0b4cd13f396?r=G&s=48&default=identicon
 
 // @include   http://www.example.com/*
