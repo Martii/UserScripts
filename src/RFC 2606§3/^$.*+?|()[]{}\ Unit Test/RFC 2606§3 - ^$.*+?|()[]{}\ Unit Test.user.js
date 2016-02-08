81945b07b599a5b739c4a7907b15347c5b5a6709
Martii <thalamew@q.com>
2016-02-08 02:33:51 -0700

    RFC 2606§3 - ^$.*+?|()[]{}\ Unit Test 0.0.0
    * Initial commit

---
+++ src/RFC 2606§3/^$.*+?|()[]{}\ Unit Test/RFC 2606§3 - ^$.*+?|()[]{}\ Unit Test.user.js
@@ -0,0 +1,21 @@
+(function () {
+
+// ==UserScript==
+// @name          RFC 2606§3 - ^$.*+?|()[]{}\ Unit Test
+// @namespace     http://localhost.localdomain
+// @description   Tests out Perl Regular expression reserved metacharacters
+// @copyright     2016+, Marti Martz (https://openuserjs.org/users/Marti)
+// @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
+// @version       0.0.0
+
+// @include   http://www.example.com/*
+// @include   http://www.example.net/*
+// @include   http://www.example.org/*
+
+// @grant                none
+
+// ==/UserScript==
+
+  // Do nothing
+
+})();
