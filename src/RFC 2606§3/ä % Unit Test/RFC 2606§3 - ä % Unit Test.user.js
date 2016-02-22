8df4e5aee93d8260250a5930746eea8ebad42a02
Martii <thalamew@q.com>
2016-02-22 00:40:27 -0700

    RFC 2606§3 - ä % Unit Test 2016.02.22.0
    * Initial commit

---
+++ src/RFC 2606§3/ä % Unit Test/RFC 2606§3 - ä % Unit Test.user.js
@@ -0,0 +1,22 @@
+(function () {
+
+// ==UserScript==
+// @name          RFC 2606§3 - ä % Unit Test
+// @namespace     https://openuserjs.org/users/Marti
+// @decription    Tests OUJS site handling of Unicode, `decodeURIComponent`, and other potentially `escape`d characters in a script name
+// @copyright     2016+, Marti Martz (https://openuserjs.org/users/Marti)
+// @license       (CC); http://creativecommons.org/licenses/by-nc-sa/3.0/
+// @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
+// @version       2016.02.22.0
+// @icon          https://www.gravatar.com/avatar/7ff58eb098c23feafa72e0b4cd13f396?r=G&s=48&default=identicon
+
+// @include   http://www.example.com/*
+// @include   http://www.example.net/*
+// @include   http://www.example.org/*
+
+// @grant       none
+// ==/UserScript==
+
+  // Doing nothing for the Unit Test
+
+})();
