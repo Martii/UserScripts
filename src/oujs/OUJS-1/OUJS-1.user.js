6c725e3b1a5fbfab9eaaa6798bdd6f136c0191fb
Martii <thalamew@q.com>
2016-05-26 14:56:14 -0600

    OUJS-1 2.0.0a.1
    * Set remote/local urls
    * Version bump

--- src/oujs/OUJS-1/OUJS-1.user.js
+++ src/oujs/OUJS-1/OUJS-1.user.js
@@ -6,11 +6,18 @@
 // @copyright    © 2014 TimidScript
 // @license      (CC); http://creativecommons.org/licenses/by-nc-sa/3.0/
 // @license      GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
-// @version      2.0.0a
+// @version      2.0.0a.1
 // @icon         https://gravatar.com/avatar/7ff58eb098c23feafa72e0b4cd13f396?s=48
 
 // @author        Marti
 // @attribution   TimidScript
+
+// @homepageURL  https://github.com/Martii/UserScripts/tree/master/src/oujs/OUJS-1
+// @homepageURL  https://openuserjs.org/scripts/Marti/OUJS-1
+// @supportURL   https://openuserjs.org/scripts/Marti/OUJS-1/issues
+
+// @updateURL    https://openuserjs.org/meta/Marti/OUJS-1.meta.js
+// @downloadURL  https://openuserjs.org/install/Marti/OUJS-1.user.js
 
 // @include  http*://openuserjs.org/*
 
