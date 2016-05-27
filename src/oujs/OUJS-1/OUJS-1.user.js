7322f031fa9bd5bdd4a135ec470618376532b0d1
Martii <thalamew@q.com>
2016-05-27 03:20:16 -0600

    OUJS-1 2.0.0a.3
    * Strip `description` and change to pending for refactor

--- src/oujs/OUJS-1/OUJS-1.user.js
+++ src/oujs/OUJS-1/OUJS-1.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         OUJS-1
 // @namespace    https://openuserjs.org/users/Marti
-// @description  New post/issue notification, adds install and ratings history stats, improves table view, list all user scripts in one page, improves library page.
+// @description  Pending rewrite
 // @copyright    © 2016+, Marti Martz, (https://openuserjs.org/users/Marti)
 // @copyright    © 2014-2016, TimidScript
 // @copyright    © 2013+, OpenUserJS Group (https://github.com/OpenUserJs)
 // @license      (CC); http://creativecommons.org/licenses/by-nc-sa/3.0/
 // @license      GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
-// @version      2.0.0a.2
+// @version      2.0.0a.3
 // @icon         https://gravatar.com/avatar/7ff58eb098c23feafa72e0b4cd13f396?s=48
 
 // @author        Marti
