e143ad3e7ee758ddcb6144f042e8aa12536232fe
Martii <thalamew@q.com>
2016-11-21 13:37:57 -0700

    RFC 2606§3 - Hello, World!
    * Retest another tag

--- src/RFC 2606§3/Hello, World/RFC 2606§3 - Hello, World.user.js
+++ src/RFC 2606§3/Hello, World/RFC 2606§3 - Hello, World.user.js
@@ -7,7 +7,7 @@
 // @copyright     2007+, Marti Martz (http://userscripts.org/users/37004)
 // @license       GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @license       (CC); http://creativecommons.org/licenses/by-nc-sa/3.0/
-// @version       0.0.0
+// @version       0.0.10
 // @icon          https://s3.amazonaws.com/uso_ss/icon/13701/large.png
 // @author        Marti Martz <somewhere@example.com> (https://openuserjs.org/users/Marti)
 
