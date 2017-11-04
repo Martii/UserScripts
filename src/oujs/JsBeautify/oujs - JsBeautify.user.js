0f3ec9529ed856ed1a5ba39098011198c4ebe473
Martii <martii@users.noreply.github.com>
2017-11-04 16:39:26 -0600

    oujs - JsBeautify 0.2.2
    * Fix my last commit... incorrect SPDX... needs `+` to indicate `or later`
    * Repoint to plain text document

--- src/oujs/JsBeautify/oujs - JsBeautify.user.js
+++ src/oujs/JsBeautify/oujs - JsBeautify.user.js
@@ -8,8 +8,8 @@
 // @copyright     2014+, Marti Martz (https://openuserjs.org/users/Marti)
 // @contributor   Chris Holland (https://github.com/Zren)
 // @license       CC-BY-NC-SA-4.0; https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode
-// @license       GPL-3.0; http://www.gnu.org/licenses/gpl-3.0-standalone.html
-// @version       0.2.1
+// @license       GPL-3.0+; http://www.gnu.org/licenses/gpl-3.0.txt
+// @version       0.2.2
 // @icon          https://gravatar.com/avatar/7ff58eb098c23feafa72e0b4cd13f396?s=48
 
 // @homepageURL  https://github.com/Martii/UserScripts/tree/master/src/oujs/JsBeautify
