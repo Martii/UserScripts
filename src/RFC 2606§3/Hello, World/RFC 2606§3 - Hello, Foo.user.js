fc92f665febec952ddd3f3e7110366b7be21e929
Martii <martii@users.noreply.github.com>
2017-11-20 20:03:44 -0700

    RFC 2606§3 - Hello, Foo! 0.0.0
    * Initial commit before foo'ing it

---
+++ src/RFC 2606§3/Hello, World/RFC 2606§3 - Hello, Foo.user.js
@@ -0,0 +1,35 @@
+(function () {
+
+// ==UserScript==
+// @name          RFC 2606§3 - Hello, Foo!
+// @namespace     http://localhost.localdomain
+// @description   JavaScript alert box saying Hello, World!
+// @copyright     2007+, Marti Martz (http://userscripts.org/users/37004)
+// @license       CC-BY-NC-SA-4.0; https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode
+// @license       GPL-3.0+; http://www.gnu.org/licenses/gpl-3.0.txt
+// @version       0.0.0
+// @icon          https://s3.amazonaws.com/uso_ss/icon/13701/large.png
+// @author        Marti Martz <somewhere@example.com> (https://openuserjs.org/users/Marti)
+
+// @homepageURL   http://userscripts.org/scripts/show/13701
+// @homepageURL   https://github.com/Martii/UserScripts/tree/master/src/RFC%202606%C2%A73/Hello%2C%20World
+// @homepageURL   https://openuserjs.org/scripts/Marti/RFC_2606%C2%A73_-_Hello,_World!
+// @supportURL    http://userscripts.org/topics/23566
+
+// @include   http://www.example.com/*
+// @include   http://www.example.net/*
+// @include   http://www.example.org/*
+
+// @updateURL https://openuserjs.org/meta/Marti/RFC_2606%C2%A73_-_Hello,_World!.meta.js
+
+// ==/UserScript==
+
+// ==OpenUserJS==
+// @author Marti
+// ==/OpenUserJS==
+
+  alert('Hello, World!');
+
+})();
+
+// GH sync test for imports
