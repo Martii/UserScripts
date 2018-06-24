808ae8a9745640b27161f0d62f3080ab795ac718
Martii <martii@users.noreply.github.com>
2018-06-23 23:10:19 -0600

    RFC 2606§3 - Hello, World! (sync test)

--- src/RFC 2606§3/Hello, World/RFC 2606§3 - Hello, World.user.js
+++ src/RFC 2606§3/Hello, World/RFC 2606§3 - Hello, World.user.js
@@ -7,7 +7,7 @@
 // @copyright     2007+, Marti Martz (http://userscripts.org/users/37004)
 // @license       CC-BY-NC-SA-4.0; https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode
 // @license       GPL-3.0+; http://www.gnu.org/licenses/gpl-3.0.txt
-// @version       0.0.1
+// @version       0.0.2
 // @icon          https://raw.githubusercontent.com/Martii/UserScripts/master/res/world.png
 // @author        Marti Martz <somewhere@example.com> (https://openuserjs.org/users/Marti)
 
