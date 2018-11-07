81e0d15702a77d9ecbb80db41310fa5e1cf9973c
Martii <martii@users.noreply.github.com>
2018-11-07 13:57:49 -0700

    RFC 2606§3 - Hello, World! (retest 0.0.1)
    * Version bump
    * Enable collaboration with one other
    * Move update URL to GH for test

--- src/RFC 2606§3/Hello, World/RFC 2606§3 - Hello, World.user.js
+++ src/RFC 2606§3/Hello, World/RFC 2606§3 - Hello, World.user.js
@@ -7,7 +7,7 @@
 // @copyright     2007+, Marti Martz (http://userscripts.org/users/37004)
 // @license       CC-BY-NC-SA-4.0; https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode
 // @license       GPL-3.0-or-later; http://www.gnu.org/licenses/gpl-3.0.txt
-// @version       0.0.0
+// @version       0.0.1
 // @icon          https://raw.githubusercontent.com/Martii/UserScripts/master/res/world.png
 // @author        Marti Martz <somewhere@example.com> (https://openuserjs.org/users/Marti)
 
@@ -22,13 +22,14 @@
 // @include   http://www.example.net/*
 // @include   http://www.example.org/*
 
-// @updateURL https://openuserjs.org/meta/Marti/RFC_2606%C2%A73_-_Hello,_World!.meta.js
+// @updateURL https://raw.githubusercontent.com/Martii/UserScripts/master/src/RFC%202606%C2%A73/Hello%2C%20World/RFC%202606%C2%A73%20-%20Hello%2C%20World.meta.js
 
 // ==/UserScript==
 
-// // ==OpenUserJS==
-// // @author Marti
-// // ==/OpenUserJS==
+// ==OpenUserJS==
+// @author Marti
+// @collaborator sizzle
+// ==/OpenUserJS==
 
   alert('Hello, World!');
 
