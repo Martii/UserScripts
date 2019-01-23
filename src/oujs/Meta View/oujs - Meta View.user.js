2c0b46fb81914475db0667bbafe430ad23b8b006
Martii <martii@users.noreply.github.com>
2019-01-22 17:24:48 -0700

    oujs - Meta View 4.4.4
    * System handled monospaced fonts for Ace ... symmetry change with OpenUserJS/OpenUserJS.org#1544

--- src/oujs/Meta View/oujs - Meta View.user.js
+++ src/oujs/Meta View/oujs - Meta View.user.js
@@ -5,7 +5,7 @@
 // @copyright     2014+, Marti Martz (https://openuserjs.org/users/Marti)
 // @license       CC-BY-NC-SA-4.0; https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode
 // @license       GPL-3.0-or-later; http://www.gnu.org/licenses/gpl-3.0.txt
-// @version       4.4.3
+// @version       4.4.4
 // @icon          https://www.gravatar.com/avatar/7ff58eb098c23feafa72e0b4cd13f396?r=G&s=48&default=identicon
 
 // @homepageURL  https://github.com/Martii/UserScripts/tree/master/src/oujs/Meta%20View
@@ -511,11 +511,13 @@
                           var mdb = thisAce.edit('mdb');
                           mdb.setTheme('ace/theme/dawn');
                           mdb.getSession().setMode('ace/mode/javascript');
+                          mdb.container.style.fontFamily = "monospace";
                           mdb.setReadOnly(true);
 
                           var mdj = thisAce.edit('json');
                           mdj.setTheme('ace/theme/dawn');
                           mdj.getSession().setMode('ace/mode/javascript');
+                          mdj.container.style.fontFamily = "monospace";
                           mdj.setReadOnly(true);
                         }
 
