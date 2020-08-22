24c0230a368b3180d817e0ca0393f5cd152d746a
Martii <martii@users.noreply.github.com>
2020-08-22 00:19:09 -0600

    Foo two scripts and see how the queue is running (or not)

--- src/RFC 2606§3/Hello, World/RFC 2606§3 - Hello, World.meta.js
+++ src/RFC 2606§3/Hello, World/RFC 2606§3 - Hello, World.meta.js
@@ -3,8 +3,8 @@
 // @namespace     http://localhost.localdomain
 // @description   JavaScript alert box saying Hello, World!
 // @copyright     2007+, Marti Martz (http://userscripts.org/users/37004)
-// @license       CC-BY-NC-SA-4.0; https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode
-// @license       GPL-3.0-or-later; http://www.gnu.org/licenses/gpl-3.0.txt
+//@license       CC-BY-NC-SA-4.0; https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode
+//@license       GPL-3.0-or-later; http://www.gnu.org/licenses/gpl-3.0.txt
 // @version       2020.08.21.12
 // @icon          https://raw.githubusercontent.com/Martii/UserScripts/master/res/world.png
 // @author        Marti Martz <somewhere@example.com> (https://openuserjs.org/users/Marti)
