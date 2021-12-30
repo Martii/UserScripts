0e73c67126af0df98230077edcf9aae23057ef3f
Martii <martii@users.noreply.github.com>
2021-12-29 19:55:40 -0700

    oujs - Meta View 4.4.14
    * Space for status code vs message

--- src/oujs/Meta View/oujs - Meta View.user.js
+++ src/oujs/Meta View/oujs - Meta View.user.js
@@ -6,7 +6,7 @@
 // @copyright     2014+, Marti Martz (https://openuserjs.org/users/Marti)
 // @license       CC-BY-NC-SA-4.0; https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode
 // @license       GPL-3.0-or-later; http://www.gnu.org/licenses/gpl-3.0.txt
-// @version       4.4.13
+// @version       4.4.14
 // @icon          https://www.gravatar.com/avatar/7ff58eb098c23feafa72e0b4cd13f396?r=G&s=48&default=identicon
 
 // @homepageURL  https://github.com/Martii/UserScripts/tree/master/src/oujs/Meta%20View
@@ -550,7 +550,7 @@
                         NodeDiv.classList.add('alert-danger');
 
                         NodeStrong.textContent = 'ERROR';
-                        NodeText.textContent = ': Unable to fetch the meta.json with status of: ' + this.status + this.statusText +
+                        NodeText.textContent = ': Unable to fetch the meta.json with status of: ' + this.status + ' ' + this.statusText +
                           (this.status === 429 ? '. Try again in ' + (this.getResponseHeader('Retry-After') ? this.getResponseHeader('Retry-After') + ' seconds.' : 'a few.') : '');
                         break;
                     }
