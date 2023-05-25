520734b5335aed7a3f4213f63d1a281cfd0eecde
Martii <martii@users.noreply.github.com>
2023-05-24 18:36:37 -0600

    OUJS-1 2.0.2a.1 - Simple 5 liner migration for newest GMC
    * An alternate method using a function expression to do the migration.
    * All it takes it five additional lines and some optional indenting of the old code for readability.
    
    Applies to sizzlemctwizzle/GM_config#113

--- src/oujs/OUJS-1/OUJS-1.user.js
+++ src/oujs/OUJS-1/OUJS-1.user.js
@@ -7,7 +7,7 @@
 // @copyright    2013+, OpenUserJS Group (https://github.com/OpenUserJs)
 // @license      CC-BY-NC-SA-4.0; https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode
 // @license      GPL-3.0-or-later; http://www.gnu.org/licenses/gpl-3.0.txt
-// @version      2.0.1a.6
+// @version      2.0.2a.1
 // @icon         https://gravatar.com/avatar/7ff58eb098c23feafa72e0b4cd13f396?s=48
 
 // @author        Marti
@@ -28,6 +28,8 @@
 
 // @grant  GM_getValue
 // @grant  GM_setValue
+// @grant  GM.getValue
+// @grant  GM.setValue
 
 // @noframes
 
@@ -105,6 +107,7 @@
 
         ].join(''),
       events: {
+        init: onInit,
         open: function (aDoc) {
           var saveNode = aDoc.querySelector('#gmc_saveBtn');
           saveNode.classList.remove('saveclose_buttons');
@@ -115,121 +118,123 @@
     }
   );
 
-  if (document.location.pathname === '/user/preferences') {
-    gmc.open();
-  }
+  function onInit() {
+    if (document.location.pathname === '/user/preferences') {
+      gmc.open();
+    }
 
-  if (gmc.get('showScriptListLineNumbers')) {
-    var count = null;
+    if (gmc.get('showScriptListLineNumbers')) {
+      var count = null;
 
-    if (document.querySelector('.pagination')) {
-      var req = new XMLHttpRequest();
-      req.responseType = 'document';
-      req.open('GET', '/?limit=1');
-      req.onreadystatechange = function () {
-        if (this.readyState == this.DONE) {
-          switch (this.status) {
-            case 200:
-            var doc = this.responseXML;
+      if (document.querySelector('.pagination')) {
+        var req = new XMLHttpRequest();
+        req.responseType = 'document';
+        req.open('GET', '/?limit=1');
+        req.onreadystatechange = function () {
+          if (this.readyState == this.DONE) {
+            switch (this.status) {
+              case 200:
+              var doc = this.responseXML;
 
-            var paginationNode = doc.querySelector('.pagination li:last-child');
-            count = parseInt(paginationNode.textContent);
+              var paginationNode = doc.querySelector('.pagination li:last-child');
+              count = parseInt(paginationNode.textContent);
 
-            var limit = document.location.search.match(/limit\=(\d+)/);
-            limit = limit ? parseInt(limit[1]) : 25;
+              var limit = document.location.search.match(/limit\=(\d+)/);
+              limit = limit ? parseInt(limit[1]) : 25;
 
-            var page = document.location.search.match(/p\=(\d+)/);
-            page = page ? parseInt(page[1]): 1;
+              var page = document.location.search.match(/p\=(\d+)/);
+              page = page ? parseInt(page[1]): 1;
 
-            var nodes = document.querySelectorAll('.col-sm-8 table .tr-link');
-            count = nodes.length;
+              var nodes = document.querySelectorAll('.col-sm-8 table .tr-link');
+              count = nodes.length;
 
-            for (var i = 0; i < count; ++i) {
-              var nodeSpan = document.createElement('span');
-              nodeSpan.classList.add('badge');
-              if (gmc.get('clipScriptListLineNumbers')) {
-                nodeSpan.textContent = (i + 1 + ((page - 1) * limit)) % 100;
-                if (nodeSpan.textContent.length === 1) {
-                    nodeSpan.textContent = '0' + nodeSpan.textContent;
+              for (var i = 0; i < count; ++i) {
+                var nodeSpan = document.createElement('span');
+                nodeSpan.classList.add('badge');
+                if (gmc.get('clipScriptListLineNumbers')) {
+                  nodeSpan.textContent = (i + 1 + ((page - 1) * limit)) % 100;
+                  if (nodeSpan.textContent.length === 1) {
+                      nodeSpan.textContent = '0' + nodeSpan.textContent;
+                  }
                 }
-              }
-              else {
-                nodeSpan.textContent = i + 1 + ((page - 1) * limit);
-              }
+                else {
+                  nodeSpan.textContent = i + 1 + ((page - 1) * limit);
+                }
 
-              nodes[i].firstChild.nextSibling.insertBefore(nodeSpan, nodes[i].firstChild.nextSibling.firstChild);
+                nodes[i].firstChild.nextSibling.insertBefore(nodeSpan, nodes[i].firstChild.nextSibling.firstChild);
+              }
+              break;
             }
-            break;
           }
-        }
-      };
-      req.send();
-    }
-    else {
-      var nodes = document.querySelectorAll('.col-sm-8 table .tr-link');
-      count = nodes.length;
+        };
+        req.send();
+      }
+      else {
+        var nodes = document.querySelectorAll('.col-sm-8 table .tr-link');
+        count = nodes.length;
 
-      for (var i = 0; i < count; ++i) {
-        var nodeSpan = document.createElement('span');
-        nodeSpan.classList.add('badge');
-        if (gmc.get('clipScriptListLineNumbers')) {
-          nodeSpan.textContent = (i + 1) % 100;
-        }
-        else {
-          nodeSpan.textContent = i + 1;
-        }
+        for (var i = 0; i < count; ++i) {
+          var nodeSpan = document.createElement('span');
+          nodeSpan.classList.add('badge');
+          if (gmc.get('clipScriptListLineNumbers')) {
+            nodeSpan.textContent = (i + 1) % 100;
+          }
+          else {
+            nodeSpan.textContent = i + 1;
+          }
 
-        nodes[i].firstChild.nextSibling.insertBefore(nodeSpan, nodes[i].firstChild.nextSibling.firstChild);
+          nodes[i].firstChild.nextSibling.insertBefore(nodeSpan, nodes[i].firstChild.nextSibling.firstChild);
+        }
       }
     }
-  }
 
-  if (gmc.get('borderImages')) {
-    var nodeStyle = document.createElement('style');
-    nodeStyle.setAttribute("type", "text/css");
-    nodeStyle.textContent = '.user-content img, .topic-post-contents img { max-width: 98%; border: 1px solid blue; padding: 2px; color: yellow; margin: 5px 0; box-shadow: 5px 5px 2px #888; }';
+    if (gmc.get('borderImages')) {
+      var nodeStyle = document.createElement('style');
+      nodeStyle.setAttribute("type", "text/css");
+      nodeStyle.textContent = '.user-content img, .topic-post-contents img { max-width: 98%; border: 1px solid blue; padding: 2px; color: yellow; margin: 5px 0; box-shadow: 5px 5px 2px #888; }';
 
-    document.head.appendChild(nodeStyle);
-  }
+      document.head.appendChild(nodeStyle);
+    }
 
-  if (gmc.get('emphasizeTableHeaderSorts')) {
-    var nodeStyle = document.createElement('style');
-    nodeStyle.setAttribute("type", "text/css");
-    nodeStyle.textContent = '.table th.active { background-color: rgba(44, 62, 80, 0.6) !important; } .table th.active a { color: white !important; }';
+    if (gmc.get('emphasizeTableHeaderSorts')) {
+      var nodeStyle = document.createElement('style');
+      nodeStyle.setAttribute("type", "text/css");
+      nodeStyle.textContent = '.table th.active { background-color: rgba(44, 62, 80, 0.6) !important; } .table th.active a { color: white !important; }';
 
-    document.head.appendChild(nodeStyle);
-  }
+      document.head.appendChild(nodeStyle);
+    }
 
-  if (gmc.get('emphasizeCurrentUsername')) {
-    var nodeStyle = document.createElement('style');
-    nodeStyle.setAttribute("type", "text/css");
-    nodeStyle.textContent = '.label-primary a { color: white !important; }';
+    if (gmc.get('emphasizeCurrentUsername')) {
+      var nodeStyle = document.createElement('style');
+      nodeStyle.setAttribute("type", "text/css");
+      nodeStyle.textContent = '.label-primary a { color: white !important; }';
 
-    document.head.appendChild(nodeStyle);
+      document.head.appendChild(nodeStyle);
 
-    // Get current username
-    var thatNode = document.querySelector('.navbar-right li:last-child');
-    var usernameNode = thatNode.previousSibling.previousSibling.firstChild;
+      // Get current username
+      var thatNode = document.querySelector('.navbar-right li:last-child');
+      var usernameNode = thatNode.previousSibling.previousSibling.firstChild;
 
-    var ownerNodes = document.querySelectorAll('span.label');
-    for (var i = 0; i < ownerNodes.length; ++i) {
-      if (ownerNodes[i].firstChild && ownerNodes[i].firstChild.textContent === usernameNode.textContent) {
-        ownerNodes[i].classList.remove('label-info');
-        ownerNodes[i].classList.add('label-primary');
+      var ownerNodes = document.querySelectorAll('span.label');
+      for (var i = 0; i < ownerNodes.length; ++i) {
+        if (ownerNodes[i].firstChild && ownerNodes[i].firstChild.textContent === usernameNode.textContent) {
+          ownerNodes[i].classList.remove('label-info');
+          ownerNodes[i].classList.add('label-primary');
+        }
       }
     }
-  }
 
-  if (gmc.get('stickMainNav')) {
-    var nodeStyle = document.createElement('style');
-    nodeStyle.setAttribute("type", "text/css");
-    nodeStyle.textContent = '.sticky { position: sticky; top: 0; }';
+    if (gmc.get('stickMainNav')) {
+      var nodeStyle = document.createElement('style');
+      nodeStyle.setAttribute("type", "text/css");
+      nodeStyle.textContent = '.sticky { position: sticky; top: 0; }';
 
-    document.head.appendChild(nodeStyle);
+      document.head.appendChild(nodeStyle);
 
-    var bodynavNode = document.querySelector('body nav');
-    if (bodynavNode) {
-      bodynavNode.classList.add('sticky');
+      var bodynavNode = document.querySelector('body nav');
+      if (bodynavNode) {
+        bodynavNode.classList.add('sticky');
+      }
     }
   }
 })();
