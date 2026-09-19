1925ea8cde68a7bfff6da85529981427563f1300
User <user@debian12.localdomain>
2026-09-19 14:29:38 -0600

    RFC 2606§3 - Hello, World! v2026.09.19.2
    * Retest webhook... migrated on OUJS, flipped on GH, and local clone manually updated

--- src/RFC 2606§3/Hello, World/RFC 2606§3 - Hello, World.meta.js
+++ src/RFC 2606§3/Hello, World/RFC 2606§3 - Hello, World.meta.js
@@ -5,13 +5,13 @@
 // @copyright     2007+, Marti Martz (http://userscripts.org/users/37004)
 // @license       CC-BY-NC-SA-4.0; https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode
 // @license       GPL-3.0-or-later; http://www.gnu.org/licenses/gpl-3.0.txt
-// @version       2026.09.19.1
-// @icon          https://raw.githubusercontent.com/Martii/UserScripts/master/res/world.png
+// @version       2026.09.19.2
+// @icon          https://raw.githubusercontent.com/Martii/UserScripts/main/res/world.png
 //@icon          https://gkeselfbilljenkins.jaas-gcp.cloud.sap.corp/static/7ce2b5b0/favicon.ico
 // @author        Marti Martz <somewhere@example.com> (https://openuserjs.org/users/Marti)
 
 // @homepageURL   http://userscripts.org/scripts/show/13701
-// @homepageURL   https://github.com/Martii/UserScripts/tree/master/src/RFC%202606%C2%A73/Hello%2C%20World
+// @homepageURL   https://github.com/Martii/UserScripts/tree/main/src/RFC%202606%C2%A73/Hello%2C%20World
 // @homepageURL   https://openuserjs.org/scripts/Marti/RFC_2606%C2%A73_-_Hello,_World!
 // @supportURL    http://userscripts.org/topics/23566
 
@@ -25,7 +25,7 @@
 // @include   https://www.example.net/*
 // @include   https://www.example.org/*
 
-// @updateURL https://raw.githubusercontent.com/Martii/UserScripts/master/src/RFC%202606%C2%A73/Hello%2C%20World/RFC%202606%C2%A73%20-%20Hello%2C%20World.meta.js
+// @updateURL https://raw.githubusercontent.com/Martii/UserScripts/main/src/RFC%202606%C2%A73/Hello%2C%20World/RFC%202606%C2%A73%20-%20Hello%2C%20World.meta.js
 
 // ==/UserScript==
 
